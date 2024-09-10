ea70bd89e331b1c6630a71e13b3cae69835b7a24
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2024-09-10 16:19:22 +0530

    Time: 186 ms (7.82%), Space: 130.5 MB (66.17%) - LeetHub

--- 2411-spiral-matrix-iv/2411-spiral-matrix-iv.cpp
+++ 2411-spiral-matrix-iv/2411-spiral-matrix-iv.cpp
@@ -11,67 +11,48 @@
 class Solution {
 public:
     vector<vector<int>> spiralMatrix(int m, int n, ListNode* head) {
-        int left = 0, right = n - 1, top = 0, bottom = m - 1;
-        vector<vector<int>> mat(m, vector<int>(n, -1));
+        int left = 0;
+        int right = n-1;
+        int top=0;
+        int bottom = m-1;
+        vector<vector<int>> v(m,vector<int>(n,-1));
 
-        ListNode* curr = head;
-        int noOfCells = 0;
-
-        while (curr != nullptr || noOfCells < m * n) {
-        
-            for (int c = left; c <= right && isSafe(left, right, top, bottom); c++) {
-                int val = -1;
-                if (curr != nullptr) {
-                    val = curr->val;
-                    curr = curr->next;
+        while(top<=bottom && left<=right){
+            for(int i = left;i<=right;i++){
+                if(head!=NULL){
+                     v[top][i] = head->val;
+                     head=head->next;
                 }
-                mat[top][c] = val;
-                noOfCells++;
             }
             top++;
-
-        
-            for (int r = top; r <= bottom && isSafe(left, right, top, bottom); r++) {
-                int val = -1;
-                if (curr != nullptr) {
-                    val = curr->val;
-                    curr = curr->next;
+            for(int i=top;i<=bottom;i++){
+                if(head!=NULL){
+                     v[i][right] = head->val;
+                     head=head->next;
                 }
-                mat[r][right] = val;
-                noOfCells++;
             }
             right--;
-
-          
-            for (int c = right; c >= left && isSafe(left, right, top, bottom); c--) {
-                int val = -1;
-                if (curr != nullptr) {
-                    val = curr->val;
-                    curr = curr->next;
+            if(top<=bottom){
+                for(int i=right;i>=left;i--){
+                    if(head!=NULL){
+                        v[bottom][i] = head->val;
+                        head=head->next;
+                    }
                 }
-                mat[bottom][c] = val;
-                noOfCells++;
+                bottom--;
             }
-            bottom--;
-
- 
-            for (int r = bottom; r >= top && isSafe(left, right, top, bottom); r--) {
-                int val = -1;
-                if (curr != nullptr) {
-                    val = curr->val;
-                    curr = curr->next;
+            
+            if(left<=right){
+                for(int i= bottom; i>=top;i--){
+                    if(head!=NULL){
+                        v[i][left] = head->val;
+                        head=head->next;
+                    }
                 }
-                mat[r][left] = val;
-                noOfCells++;
+                left++;
             }
-            left++;
+            
         }
-
-        return mat;
-    }
-
-private:
-    bool isSafe(int left, int right, int top, int bottom) {
-        return left <= right && bottom >= top;
+        return v;
     }
 };
