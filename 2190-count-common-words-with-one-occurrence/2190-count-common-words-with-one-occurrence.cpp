2bb62ce48132ce82fd4579f590e18aa1881c2079
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2024-09-17 23:43:52 +0530

    Time: 30 ms (53.93%), Space: 22.9 MB (55.53%) - LeetHub

--- 2190-count-common-words-with-one-occurrence/2190-count-common-words-with-one-occurrence.cpp
+++ 2190-count-common-words-with-one-occurrence/2190-count-common-words-with-one-occurrence.cpp
@@ -1,3 +1,31 @@
+// class Solution {
+// public:
+//     int countWords(vector<string>& words1, vector<string>& words2) {
+//         unordered_map<string,int> m1;
+//         for(int i=0;i<words1.size();i++){
+//             m1[words1[i]]++;
+//         }
+//         unordered_map<string,int> m2;
+//         for(int i=0;i<words2.size();i++){
+//             m2[words2[i]]++;
+//         }
+//         int cnt = 0;
+//         for(auto pair:m1){
+//             if(pair.second==1){
+//                 if(m2.find(pair.first)!=m2.end()){
+//                     for(auto p:m2){
+//                         if(p.first==pair.first &&  p.second==1){
+//                             cnt++;
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+
+//         return cnt;
+//     }
+// };
+
 class Solution {
 public:
     int countWords(vector<string>& words1, vector<string>& words2) {
@@ -10,15 +38,10 @@ public:
             m2[words2[i]]++;
         }
         int cnt = 0;
-        for(auto pair:m1){
-            if(pair.second==1){
-                if(m2.find(pair.first)!=m2.end()){
-                    for(auto p:m2){
-                        if(p.first==pair.first &&  p.second==1){
-                            cnt++;
-                        }
-                    }
-                }
+        
+        for(int i=0;i<words1.size();i++){
+            if(m1[words1[i]]==1  &&  m2[words1[i]]==1){
+                cnt++;
             }
         }
 
