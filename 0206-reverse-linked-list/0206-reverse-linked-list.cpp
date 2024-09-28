3d9ac1fedd5af17496d0bdf1c4ed567ecda828b5
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2024-09-29 02:14:53 +0530

    Time: 4 ms (76.04%), Space: 13.2 MB (9%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -10,35 +10,61 @@
  */
 class Solution {
 public:
-    ListNode* getNodeAt(int idx,ListNode* head){
-        ListNode* temp = head;
-        // 1 2 3 4 5;
-        for(int i=0;i<idx;i++){
-            temp=temp->next;
-        }
-        return temp;
+    // ListNode* getNodeAt(int idx,ListNode* head){
+    //     ListNode* temp = head;
+    //     // 1 2 3 4 5;
+    //     for(int i=0;i<idx;i++){
+    //         temp=temp->next;
+    //     }
+    //     return temp;
 
-    }
+    // }
+    // ListNode* reverseList(ListNode* head) {
+    //     ListNode* temp = head;
+    //     int n=0;
+    //     while(temp!=NULL){
+    //         n++;
+    //         temp=temp->next;
+    //     }
+    //     int i = 0;
+    //     int j = n-1;
+
+    //     while(i<j){
+    //         ListNode* left = getNodeAt(i,head);
+    //         ListNode* right = getNodeAt(j,head);
+    //         int t = left->val;
+    //         left->val = right->val;
+    //         right->val = t;
+    //         i++;
+    //         j--;
+    //     }
+
+    //     return head;
+    //  }
+
+    //-------------------Approach 2 (iteratively way)-------------------------------
+
+    // ListNode* reverseList(ListNode* head) {
+    //     ListNode* pre = NULL;
+    //     ListNode* cur = head;
+    //     ListNode* Next = NULL;
+    //     while(cur!=NULL){
+    //         Next = cur->next;
+    //         cur->next = pre;
+    //         pre = cur;
+    //         cur = Next;
+    //     }
+    //     return pre;
+    // }
+
+    //------------------------Recursive-------------------
+
     ListNode* reverseList(ListNode* head) {
-        ListNode* temp = head;
-        int n=0;
-        while(temp!=NULL){
-            n++;
-            temp=temp->next;
-        }
-        int i = 0;
-        int j = n-1;
+        if(head == NULL || head->next==NULL) return head;
 
-        while(i<j){
-            ListNode* left = getNodeAt(i,head);
-            ListNode* right = getNodeAt(j,head);
-            int t = left->val;
-            left->val = right->val;
-            right->val = t;
-            i++;
-            j--;
-        }
-
-        return head;
-     }
+        ListNode* newhead = reverseList(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        return newhead;
+    }
 };
