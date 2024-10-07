92146b42c3afa45ad1d4390845a01f02c563347c
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2024-10-07 23:51:00 +0530

    Time: 1347 ms (5%), Space: 177.7 MB (5.02%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
-    bool checkInclusion(string s1, string s2) 
-    {
-        unordered_map<char, int>mp, mp2;
-        for(auto ch:s1) mp2[ch]++;
+    bool check(unordered_map<char,int> m1,string str){
+        for(int i=0;i<str.size();i++){
+            if(m1.find(str[i])!=m1.end()){
+                m1[str[i]]--;
+                if(m1[str[i]]==0) m1.erase(str[i]);
+            }
+            else{
+                return false;
+            }
+        }
+        if(m1.size()==0) return true;
+        else return false;
+    }
 
-        int i = 0, j = 0, k = s1.size(), n = s2.size();
-        while(j < n)
-        {
-            mp[s2[j++]]++;
-            if(j - i == k)
-            {
-                if(mp == mp2) return true;
-                mp[s2[i]]--;
-                if(mp[s2[i]] == 0) mp.erase(s2[i]);
-                i++;
+    bool checkInclusion(string s1, string s2) 
+    {   
+        if(s2.size()<s1.size()) return false;
+        unordered_map<char,int> m1;
+        for(int i=0;i<s1.size();i++){
+            m1[s1[i]]++;
+        }
+//**i<s2.size()-s1.size()** -> ***dont use*** create problem in testcases like  s2<s1 in size
+// still we want use it to avoid TLE in testcase "bc" and "ad" then we should use
+// base case *** if(s2.size()<s1.size()) return false***
+        for(int i=0;i<s2.size()-s1.size()+1;i++){
+            string str = s2.substr(i,s1.size());//parameter2 is length of substr;
+            if(check(m1,str) == true){
+                return true;
             }
         }
         return false;
