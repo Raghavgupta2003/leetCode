141256dcba31748b64355d86866aa77921f114f0
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2024-11-09 21:06:04 +0530

    Time: 1152 ms (5.71%), Space: 107.1 MB (5.32%) - LeetHub

--- 1940-maximum-xor-for-each-query/1940-maximum-xor-for-each-query.cpp
+++ 1940-maximum-xor-for-each-query/1940-maximum-xor-for-each-query.cpp
@@ -1,13 +1,38 @@
 class Solution {
 public:
     vector<int> getMaximumXor(vector<int>& nums, int maximumBit) {
-        int n = nums.size(),xorr = nums[0],maxxorr = pow(2,maximumBit)-1;
-        for(int i=1;i<n;i++)xorr ^= nums[i];
-        vector<int>ans(n);
-        for(int i=0;i<n;i++){
-            ans[i] = xorr^maxxorr;
-            xorr ^= nums[n-1-i];
+        vector<int> prefix;
+        prefix.push_back(nums[0]);
+        for(int i=1;i<nums.size();i++){
+            int x = prefix[i-1] ^ nums[i];
+            prefix.push_back(x);
         }
+
+        for(int i=0;i<prefix.size();i++){
+           cout<<prefix[i]<<" ";
+        }
+
+        vector<int> ans;
+        int bit = pow(2,maximumBit) - 1;
+
+        // we have to find a value so that
+        // value % prefixarrayEle == pow(2,maximumBit) - 1;
+
+        //for example 
+        //nums = [0,1,1,3], maximumBit = 2
+        //for this->  bit = pow(2,maximumBit)-1 = pow(2,2)-1 = 4-1 = 3;
+        //for this prefix array is  = [0 1 0 3];
+        // value ^ 0 = 3    ---> value = 3 ^ 0 = 3;
+        // value ^ 1 = 3    ---> value = 3 ^ 1 = 2;
+        // value ^ 0 = 3    ---> value = 3 ^ 0 = 3;
+        // value ^ 3 = 3    ---> value = 3 ^ 3 = 0;
+
+        for(int i=0;i<prefix.size();i++){
+            int x = prefix[i] ^ bit;
+            ans.push_back(x);
+        }
+        reverse(ans.begin(),ans.end());
         return ans;
+
     }
 };
