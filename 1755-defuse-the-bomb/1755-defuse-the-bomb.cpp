52c33d39eaef0abddf92d1bf71d3228ba5ba19db
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2024-11-19 01:46:18 +0530

    Time: 0 ms (100%), Space: 10.5 MB (18.18%) - LeetHub

--- 1755-defuse-the-bomb/1755-defuse-the-bomb.cpp
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.cpp
@@ -1,70 +1,111 @@
+// class Solution {
+// public:
+//     vector<int> decrypt(vector<int>& code, int k) {
+//         vector<int> v;
+//         if(k==0){
+//             for(int i=0;i<code.size();i++){
+//                 v.push_back(0);
+//             }
+//         }
+//         else if(k>0){
+//             for(int i=0;i<code.size()-k;i++){
+//                 int sum = 0;
+//                 for(int j=1;j<=k;j++){
+//                     sum+=code[i+j];
+//                 }
+//                 v.push_back(sum);
+//             }
+//             vector<int> helper;
+//             for(int i=code.size()-k;i<code.size();i++){
+//                helper.push_back(code[i]);
+//             }
+//             for(int i=0;i<code.size();i++){
+//                helper.push_back(code[i]);
+//             }
+
+//             for(int i=0;i<helper.size();i++){
+//                cout<<helper[i]<<" ";
+//             }
+         
+//             for(int i=0;i<helper.size()-k;i++){
+//                 int sum = 0;
+//                 for(int j=1;j<=k;j++){
+//                     sum+=helper[i+j];
+//                 }
+//                 v.push_back(sum);
+//                 if(v.size()==code.size()) break;
+//             }
+//         }
+//         else if(k<0){
+//             reverse(code.begin(),code.end());
+//             k = abs(k);
+//              for(int i=0;i<code.size()-k;i++){
+//                 int sum = 0;
+//                 for(int j=1;j<=k;j++){
+//                     sum+=code[i+j];
+//                 }
+//                 v.push_back(sum);
+//             }
+//             vector<int> helper;
+//             for(int i=code.size()-k;i<code.size();i++){
+//                helper.push_back(code[i]);
+//             }
+//             for(int i=0;i<code.size();i++){
+//                helper.push_back(code[i]);
+//             }
+
+//             for(int i=0;i<helper.size();i++){
+//                cout<<helper[i]<<" ";
+//             }
+         
+//             for(int i=0;i<helper.size()-k;i++){
+//                 int sum = 0;
+//                 for(int j=1;j<=k;j++){
+//                     sum+=helper[i+j];
+//                 }
+//                 v.push_back(sum);
+//                 if(v.size()==code.size()) break;
+//             }
+//             reverse(v.begin(),v.end());
+//         }
+
+//         return v;
+//     }
+// };
+
+
+//--------when we need to use previous elements----then use %---------------
+// n = code.size()
+// sum+=code[j%n];
+// j is last "k" indexes of array 
 class Solution {
 public:
     vector<int> decrypt(vector<int>& code, int k) {
         vector<int> v;
+        int n = code.size();
         if(k==0){
             for(int i=0;i<code.size();i++){
                 v.push_back(0);
             }
         }
         else if(k>0){
-            for(int i=0;i<code.size()-k;i++){
-                int sum = 0;
-                for(int j=1;j<=k;j++){
-                    sum+=code[i+j];
-                }
-                v.push_back(sum);
-            }
-            vector<int> helper;
-            for(int i=code.size()-k;i<code.size();i++){
-               helper.push_back(code[i]);
-            }
             for(int i=0;i<code.size();i++){
-               helper.push_back(code[i]);
-            }
-
-            for(int i=0;i<helper.size();i++){
-               cout<<helper[i]<<" ";
-            }
-         
-            for(int i=0;i<helper.size()-k;i++){
                 int sum = 0;
-                for(int j=1;j<=k;j++){
-                    sum+=helper[i+j];
+                for(int j=i+1;j<=i+k;j++){
+                    sum+=code[j%n];
                 }
                 v.push_back(sum);
-                if(v.size()==code.size()) break;
             }
         }
         else if(k<0){
             reverse(code.begin(),code.end());
             k = abs(k);
-             for(int i=0;i<code.size()-k;i++){
+             for(int i=0;i<code.size();i++){
                 int sum = 0;
-                for(int j=1;j<=k;j++){
-                    sum+=code[i+j];
+                for(int j=i+1;j<=i+k;j++){
+                    sum+=code[j%n];
                 }
                 v.push_back(sum);
-            }
-            vector<int> helper;
-            for(int i=code.size()-k;i<code.size();i++){
-               helper.push_back(code[i]);
-            }
-            for(int i=0;i<code.size();i++){
-               helper.push_back(code[i]);
-            }
-
-            for(int i=0;i<helper.size();i++){
-               cout<<helper[i]<<" ";
-            }
-         
-            for(int i=0;i<helper.size()-k;i++){
-                int sum = 0;
-                for(int j=1;j<=k;j++){
-                    sum+=helper[i+j];
-                }
-                v.push_back(sum);
-                if(v.size()==code.size()) break;
             }
             reverse(v.begin(),v.end());
         }
