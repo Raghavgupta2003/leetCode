0168ce56e9a61319f862aa090aa22a4eb8b5654a
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2024-12-02 00:35:56 +0530

    Time: 0 ms (100%), Space: 13.7 MB (16.3%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.cpp
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.cpp
@@ -1,11 +1,23 @@
 class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
+        // for(int i=0;i<arr.size();i++){
+        //     for(int j=i+1;j<arr.size();j++){
+        //         if(arr[i] == 2*arr[j]  || arr[j] == 2*arr[i]) return true;
+        //     }
+        // }
+        // return false;
+
+        //------we can check while insering in set -----------
+
+        unordered_set<int> s;
         for(int i=0;i<arr.size();i++){
-            for(int j=i+1;j<arr.size();j++){
-                if(arr[i] == 2*arr[j]  || arr[j] == 2*arr[i]) return true;
+            if(s.find(2*arr[i])!=s.end()  || (arr[i]%2==0  && s.find(arr[i]/2)!=s.end())){
+                return true;
             }
+            s.insert(arr[i]);
         }
+        
         return false;
     }
 };
