e217fcf3cabf18214e892c86db7b90965cf0d41d
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-01-11 01:22:13 +0530

    Time: 0 ms (100%), Space: 15 MB (43.71%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -11,69 +11,19 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        // if(head->next==NULL) return NULL;
-        // ListNode* temp = head;
-        // int size=0;
-        // while(temp!=NULL){
-        //     size++;
-        //     temp = temp->next;
-        // }
-        // //cout<<size<<endl;
-        // int nodeToremove = size-n+1;
-        // //cout<<nodeToremove<<endl;
-        // int nodeToremoveIDX= nodeToremove-1; //0 based indexing
-
-        // if(nodeToremoveIDX==0){
-        //     head=head->next;
-        //     return head;
-        // }
-
-        // temp = head; //at position 1;
-        // for(int i=0;i<nodeToremoveIDX-1;i++){
-        //     temp=temp->next;
-        // }
-        // temp->next=temp->next->next;
-
-        // return head;
-
-//-----------------------------------------------------------------
-
-        // ListNode* temp = head;
-        // int size=0;
-        // while(temp!=NULL){
-        //     size++;
-        //     temp = temp->next;
-        // }
-        // if(size==n){  // if note to be removed = starting node of linkedlist
-        //     head=head->next;
-        //     return head;
-        // }
-        // int nodeToremove = size-n+1;
-        // int nodeToremoveIDX= nodeToremove-1; //0 based indexing
-
-        // temp = head;  //initially at 0;
-        // for(int i=1;i<nodeToremoveIDX;i++){
-        //     temp=temp->next;
-        // }
-        // temp->next=temp->next->next;
-
-        // return head;
-
-//-------------------OPTIMIZED_CODE----------------------------------
-      
+        
         ListNode* slow = head;
         ListNode* fast = head;
-        for(int i=1;i<=n+1;i++){
-            if(fast==NULL) return head->next; //when length == n;
-            fast=fast->next;
+        for(int i=1; i<=n+1;i++){
+            if(fast == NULL) return head->next;
+            fast = fast->next;
         }
-
         while(fast!=NULL){
             slow=slow->next;
             fast=fast->next;
         }
-        slow->next=slow->next->next;
-
+        slow->next = slow->next->next;
         return head;
     }
+
 };
