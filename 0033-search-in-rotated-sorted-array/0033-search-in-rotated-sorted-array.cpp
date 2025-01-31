ef20f3a496db5707de9ec589b8c89f1866ed19ad
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-01-31 17:28:36 +0530

    Time: 0 ms (100%), Space: 15.2 MB (66.29%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
+    int search(vector<int>& arr, int target) {
         int low = 0;
-        int high = nums.size()-1;
-        int mid = -1;
+        int high = arr.size()-1;
+
         while(low<=high){
-            mid = low + (high-low)/2;
-            if(nums[mid] == target) return mid;
-            if(nums[low]<=nums[mid]){//left half sorted
-                // is it exited in left
-                if(target>=nums[low] && target<=nums[mid]){
+            long long mid = low + ((high-low)/2);
+
+            if(arr[mid] == target) return mid;
+            else if(arr[low] <= arr[mid]){//left half is sorted
+                //checking if target lies in left sorted part
+
+                if(arr[low]<=target && arr[mid]>=target){
                     high = mid-1;
-                }else{
+                }
+                else{
                     low = mid+1;
                 }
-            }else{
-                // is it ecited in right
-                if(target>=nums[mid] && target<=nums[high]){
+
+            }
+            else{//right half is sorted
+                //checking if target lies in right sorted part
+
+                if(target>=arr[mid]  && target<=arr[high]){
                     low = mid+1;
-                }else{
+                }
+                else{
                     high = mid-1;
                 }
             }
