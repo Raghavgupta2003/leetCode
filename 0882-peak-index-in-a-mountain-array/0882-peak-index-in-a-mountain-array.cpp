c4d6eeb2ea94b1e196c910b24dd84a7b1d8f7c46
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-01-31 16:23:18 +0530

    Time: 0 ms (100%), Space: 63.7 MB (13.09%) - LeetHub

--- 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cpp
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cpp
@@ -1,10 +1,51 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        int n = arr.size()-1;
-        for(int i = 1; i<=n-1; i++){
-            if((arr[i] > arr[i-1]) && (arr[i] > arr[i+1])){
-                return i;
+        // int n = arr.size()-1;
+        // for(int i = 1; i<=n-1; i++){
+        //     if((arr[i] > arr[i-1]) && (arr[i] > arr[i+1])){
+        //         return i;
+        //     }
+        // }
+        // return 0;
+
+
+        //**********Binary Search*******
+
+        // int low = 1;
+        // int high = arr.size()-2;
+
+        // while(low<=high){
+        //     int mid = low + ((high - low)/2);
+
+        //     if((arr[mid]>arr[mid-1]) && (arr[mid]>arr[mid+1])){
+        //         return mid;
+        //     } 
+        //     else if( (arr[mid]>arr[mid-1]) && (arr[mid] < arr[mid+1]) ){
+        //         low = mid+1;
+        //     }
+        //     else if((arr[mid] < arr[mid-1]) && (arr[mid]>arr[mid+1])){
+        //         high = mid-1;
+        //     }
+        // }
+        // return 0;
+
+        //**********Binary Search***********
+
+        int low = 1;
+        int high = arr.size()-2;
+
+        while(low<=high){
+            long long mid = low + ((high - low)/2);
+
+            if((arr[mid]>arr[mid-1]) && (arr[mid]>arr[mid+1])){
+                return mid;
+            } 
+            else if(arr[mid] < arr[mid+1]){
+                low = mid+1;
+            }
+            else if(arr[mid] < arr[mid-1]){
+                high = mid-1;
             }
         }
         return 0;
