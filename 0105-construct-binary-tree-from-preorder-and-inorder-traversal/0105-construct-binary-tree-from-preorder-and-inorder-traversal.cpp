3cda704f87cefcf593d126c601492d6c37a34afe
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-02-07 19:20:49 +0530

    Time: 1 ms (84.75%), Space: 27.5 MB (56.24%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -9,28 +9,50 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     TreeNode* helper(vector<int>& preorder,vector<int>& inorder, int &preIndex, int left, int right){
+//         if(left > right) return NULL;
+
+//         TreeNode* cur = new TreeNode(preorder[preIndex]);
+//         preIndex++;
+//         int positionInInorder = -1;
+//         for(int i=left; i<=right; i++){
+//             if(inorder[i] == cur->val){
+//                 positionInInorder = i;
+//                 break;
+//             }
+//         }
+
+//         cur->left = helper(preorder, inorder, preIndex, left, positionInInorder-1);
+//         cur->right = helper(preorder, inorder, preIndex, positionInInorder + 1, right);
+//         return cur;
+//     }
+//     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+//         int preIndex = 0;
+//         return helper(preorder, inorder, preIndex, 0, inorder.size()-1);
+
+//     }
+// };
 class Solution {
 public:
-    TreeNode* helper(vector<int>& preorder,vector<int>& inorder, int &preIndex, int left, int right){
+        TreeNode* helper(vector<int>& preorder,vector<int>& inorder, int &preIndex, int left, int right, unordered_map<int, int> &hash){
         if(left > right) return NULL;
 
         TreeNode* cur = new TreeNode(preorder[preIndex]);
         preIndex++;
-        int positionInInorder = -1;
-        for(int i=left; i<=right; i++){
-            if(inorder[i] == cur->val){
-                positionInInorder = i;
-                break;
-            }
-        }
+        int positionInInorder = hash[cur->val];
 
-        cur->left = helper(preorder, inorder, preIndex, left, positionInInorder-1);
-        cur->right = helper(preorder, inorder, preIndex, positionInInorder + 1, right);
+        cur->left = helper(preorder, inorder, preIndex, left, positionInInorder-1, hash);
+        cur->right = helper(preorder, inorder, preIndex, positionInInorder + 1, right,hash);
         return cur;
     }
+  
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        unordered_map<int, int> hash;
+        for(int i=0; i<inorder.size(); i++) hash[inorder[i]] = i;
         int preIndex = 0;
-        return helper(preorder, inorder, preIndex, 0, inorder.size()-1);
+        return helper(preorder, inorder, preIndex, 0, inorder.size()-1, hash);
 
     }
 };
