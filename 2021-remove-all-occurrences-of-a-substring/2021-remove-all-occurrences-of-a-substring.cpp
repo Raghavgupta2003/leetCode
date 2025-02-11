c21dae84e8176b233af52eff780f26d02c6e5f4b
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-02-11 17:43:02 +0530

    Time: 31 ms (5.85%), Space: 31.1 MB (5.32%) - LeetHub

---
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        stack<char> st;
+        int n = part.size();
+        for(int i=0; i<s.size();i++){ 
+            if((st.size()>0) && (st.top() == part[n-1])){
+                if(st.size()>=part.size()){
+                    string x ="";
+                    int num = n;
+                    while(num>0){
+                        x = st.top() + x;
+                        num--;
+                        st.pop();
+                    }
+                    cout<<x<<" ";
+                    if(x!=part){
+                        cout<<" -----"<<x<<"------  ";
+                        for(int i=0; i<x.size();i++){
+                            st.push(x[i]);
+                        }
+                    }
+                }
+            }
+
+            st.push(s[i]);
+        }
+        if((st.size()>0) && (st.top() == part[n-1])){
+                if(st.size()>=part.size()){
+                    string x ="";
+                    int num = n;
+                    while(num>0){
+                        x = st.top() + x;
+                        num--;
+                        st.pop();
+                    }
+                    cout<<x<<" ";
+                    if(x!=part){
+                        cout<<" -----"<<x<<"------  ";
+                        for(int i=0; i<x.size();i++){
+                            st.push(x[i]);
+                        }
+                    }
+                }
+            }
+        string str = "";
+        while(st.size()>0){
+            str = (st.top()) + str;
+            st.pop();
+         }
+        return str;
+    }
+};
