8ec0933abaaba833fa80dbe49ed935eb915ad703
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-02-15 10:59:20 +0530

    Time: 0 ms (100%), Space: 77.3 MB (12%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -8,13 +8,42 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+//         int carry = 0;
+//         ListNode* farzi = new ListNode(-1);
+//         ListNode* temp = farzi;
+//         while(l1!=NULL || l2!=NULL || carry!=0){
+//             int digit1 = 0;
+//             int digit2 = 0;
+//             if(l1) digit1 = l1->val;
+//             if(l2) digit2 = l2->val;
+
+//             int sum = digit1 + digit2 + carry;
+
+//             int digit = sum % 10; //unit place digit;
+//             carry = sum/10;
+
+//             ListNode* newnode = new ListNode(digit);
+//             temp->next = newnode;
+//             temp = temp->next; 
+
+//             if(l1) l1 = l1->next;
+//             if(l2) l2 = l2->next;           
+
+//         }
+//         return farzi->next;
+//     }
+// };
+
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         int carry = 0;
         ListNode* farzi = new ListNode(-1);
         ListNode* temp = farzi;
-        while(l1!=NULL || l2!=NULL || carry!=0){
+        while(l1!=NULL || l2!=NULL){
             int digit1 = 0;
             int digit2 = 0;
             if(l1) digit1 = l1->val;
@@ -33,6 +62,15 @@ public:
             if(l2) l2 = l2->next;           
 
         }
+        if(carry!=0){
+            int sum = carry;
+            int digit = sum % 10; //unit place digit;
+            carry = sum/10;
+            ListNode* newnode = new ListNode(digit);
+            temp->next = newnode;
+            temp = temp->next; 
+        }
+
         return farzi->next;
     }
 };
