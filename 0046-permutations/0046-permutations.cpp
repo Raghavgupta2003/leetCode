d8c1fe94561f06e09f07e94f0fb8c138bfda0928
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-02-17 19:22:33 +0530

    Time: 0 ms (100%), Space: 10.6 MB (70.58%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,26 +1,49 @@
+// class Solution {
+// public:
+//     void permutations(vector<int>& arr, vector<vector<int>> &ans, vector<int>& v,  unordered_map<int, int> &m){
+//         if(v.size()== arr.size()){
+//             ans.push_back(v);
+//             return;
+//         }
+
+//         for(int i=0; i<arr.size(); i++){
+//             if(m.find(arr[i]) == m.end()){
+//                 v.push_back(arr[i]);
+//                 m[arr[i]] = 1;
+//                 permutations(arr, ans, v, m);
+//                 m.erase(arr[i]);
+//                 v.pop_back();
+//             }
+//         }
+//     }
+//     vector<vector<int>> permute(vector<int>& nums) {
+//         vector<vector<int>> ans;
+//         vector<int> v;
+//         unordered_map<int, int> m;
+//         permutations(nums, ans, v, m);
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
-    void permutations(vector<int>& arr, vector<vector<int>> &ans, vector<int>& v,  unordered_map<int, int> &m){
-        if(v.size()== arr.size()){
-            ans.push_back(v);
+    void permutations(int idx, vector<int>& arr, vector<vector<int>> &ans){
+        if(idx >= arr.size()){
+            ans.push_back(arr);
             return;
         }
 
-        for(int i=0; i<arr.size(); i++){
-            if(m.find(arr[i]) == m.end()){
-                v.push_back(arr[i]);
-                m[arr[i]] = 1;
-                permutations(arr, ans, v, m);
-                m.erase(arr[i]);
-                v.pop_back();
-            }
+        for(int i=idx; i<arr.size(); i++){
+            swap(arr[idx],arr[i]);
+            permutations(idx+1, arr, ans);
+            swap(arr[idx],arr[i]); 
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
         vector<int> v;
         unordered_map<int, int> m;
-        permutations(nums, ans, v, m);
+        permutations(0, nums, ans);
         return ans;
     }
 };
