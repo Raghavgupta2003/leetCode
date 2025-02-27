d3868486106817c3ac5533d898233a06d9d131d3
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-02-27 19:52:50 +0530

    Time: 3 ms (5.93%), Space: 10.7 MB (44.74%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,37 @@
+// class Solution {
+// public:
+//     int helper(int idx, vector<int> &nums){
+//         if(idx>=nums.size()) return 0;
+//         //pick 
+        
+//         int left = nums[idx] + helper(idx+2, nums);
+//         //not pick
+//         int right = 0 + helper(idx + 1, nums);
+
+//         return max(left,right);
+//     }
+//     int rob(vector<int>& nums) {
+//         return helper(0, nums);
+//     }
+// };
+
+
+class Solution {
+public:
+    int helper(int idx, vector<int> &nums, vector<int> &dp){
+        if(idx>=nums.size()) return 0;
+
+        if(dp[idx]!=-1) return dp[idx];
+        //pick 
+        int left = nums[idx] + helper(idx+2, nums,dp);
+        //not pick
+        int right = 0 + helper(idx + 1, nums,dp);
+
+        return dp[idx] = max(left,right);
+    }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n+1,-1);
+        return helper(0, nums,dp);
+    }
+};
