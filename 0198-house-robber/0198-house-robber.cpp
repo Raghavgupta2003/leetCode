6c28d5b39a69835ed6075e9e38cc599782ce44c0
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-02-27 20:50:40 +0530

    Time: 0 ms (100%), Space: 10.9 MB (5.87%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -15,23 +15,50 @@
 //     }
 // };
 
+//---------------Memorization-------------------
+
+
+// class Solution {
+// public:
+//     int helper(int idx, vector<int> &nums, vector<int> &dp){
+//         if(idx>=nums.size()) return 0;
+
+//         if(dp[idx]!=-1) return dp[idx];
+//         //pick 
+//         int left = nums[idx] + helper(idx+2, nums,dp);
+//         //not pick
+//         int right = 0 + helper(idx + 1, nums,dp);
+
+//         return dp[idx] = max(left,right);
+//     }
+//     int rob(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int> dp(n,-1);
+//         return helper(0, nums,dp);
+//     }
+// };
+
+//---------------Tabulation-------------------
+
 
 class Solution {
 public:
-    int helper(int idx, vector<int> &nums, vector<int> &dp){
-        if(idx>=nums.size()) return 0;
-
-        if(dp[idx]!=-1) return dp[idx];
-        //pick 
-        int left = nums[idx] + helper(idx+2, nums,dp);
-        //not pick
-        int right = 0 + helper(idx + 1, nums,dp);
-
-        return dp[idx] = max(left,right);
-    }
     int rob(vector<int>& nums) {
         int n = nums.size();
         vector<int> dp(n,-1);
-        return helper(0, nums,dp);
+
+        dp[0] = nums[0];
+        if(nums.size() > 1) dp[1] =  max(nums[0],nums[1]);
+        
+        for(int i=2; i<dp.size(); i++){
+            //pick
+            int left = nums[i] + dp[i-2];
+            //not pick
+            int right = dp[i-1];
+
+            dp[i] = max(left,right);
+        }
+
+        return dp[n-1];
     }
 };
