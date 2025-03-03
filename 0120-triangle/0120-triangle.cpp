b46234de661ed6a5cb84710beb05152a4c6dcbd2
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-03-04 00:54:55 +0530

    Time: 0 ms (100%), Space: 13.1 MB (46.4%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -17,6 +17,29 @@
 //----------------------MEMORIZATION--------------------------
 
 
+// class Solution {
+// public:
+//     int path(int row, int col, vector<vector<int>>& triangle, vector<vector<int>> &dp){
+//         if(row >= triangle.size()) return 0;
+        
+//         if(dp[row][col] != -1) return dp[row][col];
+//         int down = triangle[row][col] + path(row+1, col, triangle, dp);
+//         int diagonal =  triangle[row][col] + path(row+1, col+1, triangle, dp);
+
+//         return dp[row][col] = min(down, diagonal);
+
+//     }
+//     int minimumTotal(vector<vector<int>>& triangle) {
+//         int n = triangle.size();
+//         vector<vector<int>> dp(n, vector<int>(n, -1));
+//         return path(0,0,triangle,dp);
+//     }
+// };
+
+
+//---------------------Tabulation--------------------------
+
+
 class Solution {
 public:
     int path(int row, int col, vector<vector<int>>& triangle, vector<vector<int>> &dp){
@@ -32,6 +55,20 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
         vector<vector<int>> dp(n, vector<int>(n, -1));
-        return path(0,0,triangle,dp);
+        //basecase
+        for(int i=0; i<n; i++){
+            dp[n-1][i] = triangle[n-1][i];
+        }
+        //dp
+        for(int i= n-2; i>=0; i--){
+            for(int j=i; j>=0; j--){//j=i as its triangle
+                int down = triangle[i][j] + dp[i+1][j];
+                int diagonal = triangle[i][j] + dp[i+1][j+1];
+
+                dp[i][j] = min(down, diagonal);
+            }
+        }
+
+        return dp[0][0];
     }
 };
