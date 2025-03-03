733b8cb487fb62ef99d69f1a33d6a97a50ad74d0
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-03-04 00:31:56 +0530

    Time: 0 ms (100%), Space: 13.2 MB (46.4%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,56 +1,37 @@
 // class Solution {
 // public:
-//     int minpath(int idx, int last, vector<vector<int>>& triangle){
-//         if(idx >= triangle.size()) return 0;
+//     int path(int row, int col, vector<vector<int>>& triangle){
+//         if(row >= triangle.size()) return 0;
         
-//         int mini = 1e9;
-//         for(int i=last; i<triangle[idx].size(); i++){
-//             int x = triangle[idx][i] + minpath(idx+1, i, triangle);
-//             mini = min(mini, x);
-//         }
-//         return mini;
-//     }
-//     int minimumTotal(vector<vector<int>>& triangle) {
-        
-//         return minpath(0,0,triangle);
-        
-//     }
-// };
+//         int down = triangle[row][col] + path(row+1, col, triangle);
+//         int diagonal =  triangle[row][col] + path(row+1, col+1, triangle);
 
-
-// class Solution {
-// public:
-//     int minpath(int idx, int col, vector<vector<int>>& triangle){
-//         if(idx >= triangle.size()) return 0;
-        
-//         int down = triangle[idx][col] + minpath(idx+1, col, triangle);
-//         int diagonal = triangle[idx][col] + minpath(idx+1, col+1, triangle);
 //         return min(down, diagonal);
+
 //     }
 //     int minimumTotal(vector<vector<int>>& triangle) {
-        
-//         return minpath(0,0,triangle);
-        
+//         return path(0,0,triangle);
 //     }
 // };
 
-//-------------------MEMORIZATION----------------
+//----------------------MEMORIZATION--------------------------
+
 
 class Solution {
 public:
-    int minpath(int idx, int col, vector<vector<int>>& triangle, vector<vector<int>> &dp){
-        if(idx >= triangle.size()) return 0;
+    int path(int row, int col, vector<vector<int>>& triangle, vector<vector<int>> &dp){
+        if(row >= triangle.size()) return 0;
         
-        if(dp[idx][col] != -1) return dp[idx][col];
-       
-        int down = triangle[idx][col] + minpath(idx+1, col, triangle,dp);
-        int diagonal = triangle[idx][col] + minpath(idx+1, col+1, triangle,dp);
-        return dp[idx][col] = min(down, diagonal);
+        if(dp[row][col] != -1) return dp[row][col];
+        int down = triangle[row][col] + path(row+1, col, triangle, dp);
+        int diagonal =  triangle[row][col] + path(row+1, col+1, triangle, dp);
+
+        return dp[row][col] = min(down, diagonal);
+
     }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
         vector<vector<int>> dp(n, vector<int>(n, -1));
-        return minpath(0,0,triangle,dp);
-        
+        return path(0,0,triangle,dp);
     }
 };
