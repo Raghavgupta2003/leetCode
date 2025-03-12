d941a5fd8b888c74455aed65d5ff5a0c9c45bcfb
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-03-12 20:42:38 +0530

    Time: 0 ms (100%), Space: 21.5 MB (10.71%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -1,27 +1,63 @@
 class Solution {
 public:
     int maximumCount(vector<int>& nums) {
-        // int low = 0;
-        // int high = nums.size()-1;
-        // while(low<=high){
-        //     int mid = low + (high-low)/2;
+        int low = 0;
+        int high = nums.size()-1;
+        int lastNegativeIdx = -1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
 
-        //     if(nums[mid] == 0){
-                
-        //     }
-        //     else if(nums[mid]<0){
-        //         high = mid-1;
-        //     }
-        //     else if(nums[mid]>0){
-        //         low = mid + 1;
-        //     }
-        //}
-        int pos = 0;
-        int neg = 0;
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i]>0) pos++;
-            if(nums[i]<0) neg++;
+            if(nums[mid] < 0){
+                if(mid == nums.size()-1){
+                    lastNegativeIdx = mid;
+                    break;
+                }
+                if(mid!=nums.size()-1 && nums[mid+1] >=0){
+                    lastNegativeIdx = mid;
+                    break;
+                }
+                else low = mid + 1;
+            }
+            else if(nums[mid]>=0){
+                high = mid-1;
+            }
+            
         }
-        return max(pos,neg);
+        cout<<lastNegativeIdx<<" ";
+        long long neg = lastNegativeIdx + 1;
+
+       //-----------------------------------------------
+
+        low = 0;
+        high = nums.size()-1;
+        long long firstPositiveIdx = -1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+
+            if(nums[mid] > 0){
+                if(mid == 0){
+                    firstPositiveIdx = mid;
+                    break;
+                }
+                if(mid!=0 && nums[mid-1] <=0){
+                    firstPositiveIdx = mid;
+                    break;
+                }
+                else high = mid - 1;
+            }
+            else if(nums[mid]<=0){
+                low = mid+1;
+            }
+            
+        }
+        cout<<firstPositiveIdx<<" ";
+        if(firstPositiveIdx == -1) return neg;
+        long long pos = nums.size() - (firstPositiveIdx);
+        cout<<pos;
+        //return pos;
+
+        return max(neg, pos);
+
+        
     }
 };
