a2f5b0efd505cc3b7681e085e4f5e200bf109b0b
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-03-18 20:27:24 +0530

    Time: 0 ms (100%), Space: 60.9 MB (87.7%) - LeetHub

--- 2478-longest-nice-subarray/2478-longest-nice-subarray.cpp
+++ 2478-longest-nice-subarray/2478-longest-nice-subarray.cpp
@@ -1,24 +1,50 @@
+// class Solution {
+// public:
+//     bool isNice(int i, int j, vector<int>& nums){
+//         int mask = 0;
+//         for(int k = i; k <= j; k++){
+//             if((mask & nums[k]) != 0) return false;  //condition met
+//             mask = mask | nums[k];  // storing whose bits are 1
+//         }
+//         return true;
+//     }
+//     int longestNiceSubarray(vector<int>& nums) {
+//         int i=0;
+//         int j=0;
+//         int maxi =0;
+//         while(j<nums.size()){
+//            if(isNice(i, j, nums) == false){
+//                 i++;
+//            }
+//            else{
+//                 maxi = max(maxi, j-i+1);
+//             }
+//            j++;
+//         }
+
+//         return maxi;
+//     }
+// };
+
+
+
+//----------------------------------
+
+
 class Solution {
 public:
-    bool isNice(int i, int j, vector<int>& nums){
-        int mask = 0;
-        for(int k = i; k <= j; k++){
-            if((mask & nums[k]) != 0) return false;  //condition met
-            mask = mask | nums[k];  // storing whose bits are 1
-        }
-        return true;
-    }
     int longestNiceSubarray(vector<int>& nums) {
         int i=0;
         int j=0;
         int maxi =0;
+        int mask = 0;
         while(j<nums.size()){
-           if(isNice(i, j, nums) == false){
+           while((mask & nums[j]) != 0){ 
+                mask = mask ^ nums[i]; //removing influence of i
                 i++;
            }
-           else{
-                maxi = max(maxi, j-i+1);
-            }
+            maxi = max(maxi, j-i+1);
+            mask = mask | nums[j];
            j++;
         }
 
