487d5d4c6a3dff1db6092e4c80a7e5f9a2b4f50e
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-03-19 01:45:52 +0530

    Time: 17 ms (64.9%), Space: 28.7 MB (38.57%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int helper(int idx, int amount, vector<int>& coins, vector<vector<int>>& dp){
+        if(amount == 0) return 1;
+        if(idx<0) return 0;
+
+         if(dp[idx][amount] != -1)  return dp[idx][amount];
+
+        int pick = 0;
+        if(amount >= coins[idx]) pick = helper(idx, amount-coins[idx], coins, dp);
+
+        int notpick = helper(idx-1, amount, coins, dp);
+
+        return dp[idx][amount] = pick + notpick;
+
+    }
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int> (amount+1, -1));
+
+        return helper(n-1, amount, coins, dp);
+    }
+};
