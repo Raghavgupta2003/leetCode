45885874420ca5877312de7871cfbdfcb70bfc0f
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-03-18 19:57:53 +0530

    Time: 4 ms (38.81%), Space: 61 MB (28.44%) - LeetHub

---
+++ 2478-longest-nice-subarray/2478-longest-nice-subarray.cpp
@@ -0,0 +1,31 @@
+
+//--------------O(n^2)--------------------
+
+class Solution {
+public:
+    bool isNice(int i, int j, vector<int>& nums){
+        int mask = 0;
+        for(int k = i; k <= j; k++){
+            if((mask & nums[k]) != 0) return false;  //condition met
+            mask = mask | nums[k];  // check count of bits which are 1
+            if(mask == 0) return false;
+        }
+        return true;
+    }
+    int longestNiceSubarray(vector<int>& nums) {
+        int i=0;
+        int j=0;
+        int maxi =0;
+        while(j<nums.size()){
+           if(isNice(i, j, nums) == false){
+                i++;
+           }
+           else{
+                maxi = max(maxi, j-i+1);
+            }
+           j++;
+        }
+
+        return maxi;
+    }
+};
