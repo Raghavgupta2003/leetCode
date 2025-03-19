0d94632d7875dfeb1453c4c22940d6d8f198e688
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-03-19 11:23:19 +0530

    Time: 0 ms (100%), Space: 34.3 MB (63.33%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -14,33 +14,30 @@ public:
     TreeNode* deleteNode(TreeNode* root, int key) {
         if(root == NULL) return NULL;
 
-        if(key < root->val){
-            root->left = deleteNode(root->left, key);
-        }
-        else if(key > root->val){
+        if(root->val < key){
             root->right = deleteNode(root->right, key);
         }
-        else{ //root->val == key
-
-        if(root->left == NULL && root->right == NULL) return NULL; //leafNode;
-        else if(root->left!=NULL && root->right == NULL) return root->left; //having only left child;
-        else if(root->left==NULL && root->right!=NULL) return root->right; //having only right child;
-        else{//having both child;
-
-        TreeNode* subtree = root->right;
-        TreeNode* subtreetemp = subtree;
-        while(subtreetemp->left!=NULL){
-            subtreetemp = subtreetemp->left;
+        else if(root->val > key){
+            root->left = deleteNode(root->left, key);
         }
-        subtreetemp->left = root->left;
+        else{
+            if(root->left == NULL && root->right == NULL) return NULL;
+            else if(root->left == NULL && root->right!=NULL) return root->right;
+            else if(root->left != NULL && root->right==NULL) return root->left;
+            else{
+                TreeNode* subtree = root->right;
+                TreeNode* subtemp = subtree;
 
-        return subtree;
+                while(subtemp->left !=NULL){
+                    subtemp = subtemp->left;
+                }
+                subtemp->left = root->left;
 
-
-
+                return subtree;
+            }
         }
 
-        }
+
         return root;
     }
 };
