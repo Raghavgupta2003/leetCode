e5ed86f8cbf5e10af3cf37ccb03a81fb55f96c45
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-03-25 04:48:18 +0530

    Time: 3 ms (14.11%), Space: 19.4 MB (6.3%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,14 +1,41 @@
+// class Solution {
+// public:
+//    void dfs(vector<vector<int>>& matrix, int i, int j, int prevColor, int color){
+//     if(i<0 || i>=matrix.size() || j>=matrix[0].size() || j<0) return;
+//     if (matrix[i][j] != prevColor || matrix[i][j] == color) return;
+
+//     if(matrix[i][j] == prevColor) matrix[i][j] = color;
+//     dfs(matrix, i+1, j, prevColor, color);
+//     dfs(matrix, i, j+1, prevColor, color);
+//     dfs(matrix, i-1, j, prevColor, color);
+//     dfs(matrix, i, j-1, prevColor, color);
+//    }
+//     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+//         vector<vector<int>> matrix = image;
+//         int i=sr;
+//         int j=sc;
+//         int prevColor = matrix[i][j];
+
+//         dfs(matrix, i, j, prevColor, color);
+
+//         return matrix;
+        
+//     }
+// };
+
+//------------------------------------------------------------------------------------
+
 class Solution {
 public:
-   void dfs(vector<vector<int>>& matrix, int i, int j, int prevColor, int color){
+   void dfs(vector<vector<int>>& matrix, int i, int j, int prevColor, int color,  vector<int> delRow,  vector<int> delcol){
     if(i<0 || i>=matrix.size() || j>=matrix[0].size() || j<0) return;
     if (matrix[i][j] != prevColor || matrix[i][j] == color) return;
 
     if(matrix[i][j] == prevColor) matrix[i][j] = color;
-    dfs(matrix, i+1, j, prevColor, color);
-    dfs(matrix, i, j+1, prevColor, color);
-    dfs(matrix, i-1, j, prevColor, color);
-    dfs(matrix, i, j-1, prevColor, color);
+
+    for(int k=0; k<4; k++){
+        dfs(matrix, i+delRow[k], j+delcol[k], prevColor, color, delRow, delcol);
+    }
    }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         vector<vector<int>> matrix = image;
@@ -16,7 +43,10 @@ public:
         int j=sc;
         int prevColor = matrix[i][j];
 
-        dfs(matrix, i, j, prevColor, color);
+
+        vector<int> delRow = {0, 1, 0, -1};
+        vector<int> delcol = {1, 0, -1, 0};
+        dfs(matrix, i, j, prevColor, color, delRow, delcol);
 
         return matrix;
         
