8870cc67c398ead1e1d8a0dfe5102303c5670211
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-03-26 05:24:47 +0530

    Time: 95 ms (5.01%), Space: 23.1 MB (98.94%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,32 +1,63 @@
+//-----------------------by using 2 stacks---------------------
+// class MinStack {
+// public:
+//     stack<int> st;
+//     stack<int> helper;
+//     MinStack() {
+        
+//     }
+    
+//     void push(int val) {
+//         st.push(val);
+
+//         if(helper.size() == 0) helper.push(val); // initially
+//         else{
+//             if(val < helper.top()) helper.push(val); // pushhing minimum
+//             else helper.push(helper.top()); // if val is not minimum then previous one is minimum.
+//         } 
+//     }
+    
+//     void pop() {
+//         if(st.size()>0) st.pop();
+//         if(helper.size()>0) helper.pop();
+//     }
+    
+//     int top() {
+//         return st.top();
+//     }
+    
+//     int getMin() {
+//         return helper.top();
+//     }
+// };
+
+//------------------------------approach 2 by vector----------------------
+
 class MinStack {
 public:
-    stack<int> st;
-    stack<int> helper;
+    vector<int> v;
     MinStack() {
         
     }
     
     void push(int val) {
-        st.push(val);
-
-        if(helper.size() == 0) helper.push(val);
-        else{
-            if(val < helper.top()) helper.push(val);
-            else helper.push(helper.top());
-        } 
+        v.push_back(val);
     }
     
     void pop() {
-        if(st.size()>0) st.pop();
-        if(helper.size()>0) helper.pop();
+        v.pop_back();
     }
     
     int top() {
-        return st.top();
+        return v[v.size()-1];
     }
     
     int getMin() {
-        return helper.top();
+        int mini = INT_MAX;
+        for(int i=0; i<v.size(); i++){
+            if(v[i] < mini) mini = v[i];
+        }
+        return mini;
     }
 };
 
