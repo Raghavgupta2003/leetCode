8be3dbbb61eb9311861f0f86f315d33501888ccb
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-04-11 00:33:43 +0530

    Time: 0 ms (100%), Space: 27.6 MB (65.87%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -1,22 +1,51 @@
+// class Solution {
+// public:
+//     int minimumOperations(vector<int>& nums) {
+//         // we have basecase for size = 0, 1, 2
+//         if(nums.size() == 0) return 0; //already distint;
+//         if(nums.size() == 1) return 0;
+//         if(nums.size() == 2){
+//             if(nums[0] == nums[1]) return 1;
+//             return 0;
+//         }
+//         int cnt = 0;
+//         int i=0;
+//         for(i=0; i<nums.size(); i+=3){
+//             set<int> s;
+//             for(int j=i; j<nums.size(); j++){
+//                 s.insert(nums[j]);
+//             }
+//             if(s.size() == nums.size()-i) return cnt;
+//             cnt++;
+//         }
+//         return cnt;
+
+//         //we use set and nested loop to remove duplicate
+//         //we increment i by 3.
+//     }
+// };
+
+
+//------------------------------------------------------------------
+
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
-        if(nums.size() == 0) return 0; //already distint;
-        if(nums.size() == 1) return 0;
-        if(nums.size() == 2){
-            if(nums[0] == nums[1]) return 1;
-            return 0;
-        }
-        int cnt = 0;
-        int i=0;
-        for(i=0; i<nums.size(); i+=3){
-            set<int> s;
-            for(int j=i; j<nums.size(); j++){
-                s.insert(nums[j]);
-            }
-            if(s.size() == nums.size()-i) return cnt;
-            cnt++;
-        }
-        return cnt;
+       // we use hashmap and store digits from end of array.
+       //if frquency of any element become 2 or greater
+       //break the loop then we find number of operation by value of i;
+
+        unordered_map<int, int> m;
+        int i;
+       for(i=nums.size()-1; i>=0; i--){
+             m[nums[i]]++;
+            if(m[nums[i]] >= 2) break;
+       }
+       cout<<i;
+
+       if(i == -1) return 0; //no duplicate found
+       if((i+1)%3 == 0) return (i+1)/3;
+       
+       return ((i+1)/3)+1;
     }
 };
