c54003d2ab1777ee542b0355f44e50b7895ad0fc
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-04-11 01:35:44 +0530

    Time: 13 ms (37.41%), Space: 31.2 MB (84.32%) - LeetHub

--- 3621-minimum-operations-to-make-array-values-equal-to-k/3621-minimum-operations-to-make-array-values-equal-to-k.cpp
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/3621-minimum-operations-to-make-array-values-equal-to-k.cpp
@@ -1,18 +1,34 @@
 class Solution {
 public:
+    
     int minOperations(vector<int>& nums, int k) {
+        //basecase
+        //when any element is less than k, we return -1
+        //because we are not able to convert all elements equal to k.
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]<k) return -1;
+        }
+
+        //sort to find valid h
+        int n = nums.size();
         sort(nums.begin(), nums.end());
-        int mini = nums[0];
-        if (mini < k) return -1; // edge case
-        
         int cnt = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] > k) {
+
+        // selecting valid h and replace all greater than it by h.
+        for(int i=nums.size()-1; i>0; i--){
+            if(nums[i]!=nums[i-1]){
+                int h = nums[i-1];
+                for(int j=i; j<nums.size(); j++){
+                    nums[j] = h;
+                }
                 cnt++;
-                while (i + 1 < nums.size() && nums[i] == nums[i + 1]) 
-                    i++;
             }
         }
+
+        if(nums[0] > k) cnt = cnt+1; // this is a case when our all elements become equal but less greater than k. In this case, our h = k.
+        
+        if(k > nums[0]) return -1; //this is case when we are not able to make our array equal to k.
         return cnt;
+        
     }
 };
