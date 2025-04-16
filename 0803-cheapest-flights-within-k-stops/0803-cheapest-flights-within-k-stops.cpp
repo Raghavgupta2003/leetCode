773e82c37752f2ae4ffe67ffb598e18f7fab87d0
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-04-17 01:47:28 +0530

    Time: 0 ms (100%), Space: 17.6 MB (80.87%) - LeetHub

---
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    //dijkstra
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<vector<pair<int,int>>> adj(n, vector<pair<int,int>>(0));
+
+        for(int i=0; i<flights.size(); i++){
+            int a = flights[i][0];
+            int b = flights[i][1];
+            int w = flights[i][2];
+
+            adj[a].push_back({b, w});
+        }
+
+        vector<int> distance(n, 1e9);
+
+        priority_queue<pair<int,pair<int, int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int, int>>>> pq;
+
+        pq.push({1, {0, src}});  //{stop, {distance, node}}  stops = number_of_nodes -2 (number_of_nodes - start_node - end_node) 
+        distance[src] = 0;
+        
+
+        int mini = INT_MAX;
+        while(pq.size() > 0){
+           int stop = pq.top().first;
+           int dist = pq.top().second.first;
+           int node = pq.top().second.second;
+
+           if(node == dst && stop <= k+2){
+                mini = min(mini, dist);
+                // there can be many ways to reach destination in which stops <=k+2
+                //but we need cheapest
+                //so, we take minimum of all paths.
+           }
+
+           pq.pop();
+
+           for(auto it : adj[node]){
+                int adjNode = it.first;
+                int adjDist = it.second;
+
+                if(distance[adjNode] > adjDist + dist){
+                    distance[adjNode] = adjDist + dist;
+                    pq.push({stop+1, {adjDist + dist,adjNode}}); //increase the stop by 1
+                }
+           }
+        }
+        if(mini == INT_MAX) return -1;
+        return mini;
+    }
+};
