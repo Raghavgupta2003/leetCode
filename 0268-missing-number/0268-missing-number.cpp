0987f530aa9573a7a48542e8ac1b1a5aea9db82b
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-05-27 00:36:30 +0530

    Time: 11 ms (12.38%), Space: 21.9 MB (38.26%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,37 +1,12 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        // sort(nums.begin(),nums.end());
-        // int n = nums.size();
+        sort(nums.begin(), nums.end());
 
-        // int low = 0;
-        // int high = n-1;
-        // int ans = -1;
-        // while(low<=high){
-        //     int mid = low + ((high - low)/2);
-        //     if(nums[mid] == mid){
-        //         low = mid+1;
-        //     }
-        //     else{
-        //         ans = mid;
-        //         high = mid-1;
-        //     }
-        // }
-        // if(ans == -1) return nums[nums.size()-1]+1;
-        // return ans;
-
-
-        //-----------------approach2 -> (sum of n elements - sum of array) 
-
-        int n = nums.size();
-        int sum = (n*(n+1))/2; 
-        int sumarr = 0;
-        for(int i=0;i<n;i++){
-            sumarr+=nums[i];
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] != i) return i;
         }
 
-        return sum - sumarr;
-
-
+        return nums.size(); //[0, 1] -> missing last element that is 2
     }
 };
