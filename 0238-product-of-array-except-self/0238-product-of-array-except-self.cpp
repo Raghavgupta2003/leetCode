bda76bcebb5ecc813bf12deeea5f199b95d4a312
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-06-07 10:06:41 +0530

    Time: 3 ms (39.1%), Space: 42.3 MB (16.93%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,51 +1,21 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-
-        //-----by division operator-----
-
-        // int pdt=1;
-        // int pdt1=1;
-        // int noz = 0;
-        // for(int i=0;i<nums.size();i++){
-        //     if(nums[i]==0) noz++;
-
-        //     pdt*=nums[i];
-
-        //     if(nums[i]!=0) pdt1*=nums[i];
-        //     else continue;
-        // }
-        // if(noz>1) pdt1=0;
-        // for(int i=0;i<nums.size();i++){
-        //     if(nums[i]!=0)
-        //     nums[i] = pdt/nums[i];
-        //     else
-        //     nums[i]=pdt1;
-        // }
-        // return nums;
-
-        //------------------------------------------
-        int n=nums.size();
-
-        //pre array
-        vector<int> pre(n);
-        pre[0]=1;
-        for(int i=1;i<n;i++){
-            pre[i]=pre[i-1]*nums[i-1];
+        int n = nums.size();
+        vector<int> pre(n, 1);
+        for(int i=1; i<nums.size(); i++){
+            pre[i] = pre[i-1] * nums[i-1];
         }
 
-        //suf array
-        vector<int> suf(n);
-        suf[n-1]=1;
-        for(int i=n-2;i>=0;i--){
-            suf[i]=suf[i+1]*nums[i+1];
+        vector<int> post(n, 1);
+        for(int i=n-2; i>=0; i--){
+            post[i] = post[i+1] * nums[i+1];
         }
-        
 
-        vector<int> ans;
-        for(int i=0;i<n;i++){
-            ans.push_back(pre[i]*suf[i]);
+        for(int i=0; i<nums.size(); i++){
+            nums[i] = pre[i]*post[i];
         }
-        return ans;
+
+        return nums;
     }
 };
