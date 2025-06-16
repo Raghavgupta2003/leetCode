16f710a4b2ae70b23efa2c5e826efc5ba068b5e4
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-06-16 11:33:32 +0530

    Time: 0 ms (100%), Space: 15.1 MB (66.57%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,36 +1,32 @@
 class Solution {
 public:
-    int search(vector<int>& arr, int target) {
+    int search(vector<int>& nums, int target) {
         int low = 0;
-        int high = arr.size()-1;
+        int high = nums.size()-1;
 
-        while(low<=high){
-            long long mid = low + ((high-low)/2);
-
-            if(arr[mid] == target) return mid;
-            else if(arr[low] <= arr[mid]){//left half is sorted
-                //checking if target lies in left sorted part
-
-                if(arr[low]<=target && arr[mid]>=target){
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            
+            
+            if(nums[mid] == target) return mid;
+            else if(nums[low] <= nums[mid]){//left sorted
+                if(nums[low]<=target && target<=nums[mid]){
+                    //we will reduce space
                     high = mid-1;
                 }
                 else{
-                    low = mid+1;
+                    low = mid + 1;
                 }
-
-            }
-            else{//right half is sorted
-                //checking if target lies in right sorted part
-
-                if(target>=arr[mid]  && target<=arr[high]){
+            }else{ //right sorted
+                if(nums[mid]<=target && target<=nums[high]){
+                    //reduce space
                     low = mid+1;
                 }
                 else{
-                    high = mid-1;
+                    high = mid - 1;
                 }
             }
         }
-
         return -1;
     }
 };
