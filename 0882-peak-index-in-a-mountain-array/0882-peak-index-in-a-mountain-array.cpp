a3e3e4134d57fa092fdabb1c9c4162922aca5213
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-06-18 10:53:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cpp
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cpp
@@ -1,53 +1,24 @@
 class Solution {
 public:
-    int peakIndexInMountainArray(vector<int>& arr) {
-        // int n = arr.size()-1;
-        // for(int i = 1; i<=n-1; i++){
-        //     if((arr[i] > arr[i-1]) && (arr[i] > arr[i+1])){
-        //         return i;
-        //     }
-        // }
-        // return 0;
-
-
-        //**********Binary Search*******
-
-        // int low = 1;
-        // int high = arr.size()-2;
-
-        // while(low<=high){
-        //     int mid = low + ((high - low)/2);
-
-        //     if((arr[mid]>arr[mid-1]) && (arr[mid]>arr[mid+1])){
-        //         return mid;
-        //     } 
-        //     else if( (arr[mid]>arr[mid-1]) && (arr[mid] < arr[mid+1]) ){
-        //         low = mid+1;
-        //     }
-        //     else if((arr[mid] < arr[mid-1]) && (arr[mid]>arr[mid+1])){
-        //         high = mid-1;
-        //     }
-        // }
-        // return 0;
-
-        //**********Binary Search***********
-
-        int low = 1;
-        int high = arr.size()-2;
+    int peakIndexInMountainArray(vector<int>& nums) {
+        int n = nums.size();
+        int low = 0;
+        int high = n-1;  
 
         while(low<=high){
-            long long mid = low + ((high - low)/2);
+            int mid = low + (high-low)/2;
 
-            if((arr[mid]>arr[mid-1]) && (arr[mid]>arr[mid+1])){
+            if(nums[mid-1] < nums[mid] && nums[mid] > nums[mid+1]){
                 return mid;
-            } 
-            else if(arr[mid] < arr[mid+1]){
-                low = mid+1;
             }
-            else if(arr[mid] < arr[mid-1]){
-                high = mid-1;
+            else if(nums[mid] < nums[mid+1]){ //right is increasing
+                low = mid+1; //right search
+            }
+            else if(nums[mid] > nums[mid+1]){ //right is decreasing
+                high = mid-1; //left search
             }
         }
-        return 0;
+
+        return -1;
     }
 };
