9534ae9eb3c7bf6f2ed6f69c68e3c3170c3f362a
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-06-19 11:43:40 +0530

    Time: 0 ms (100%), Space: 15.7 MB (33.01%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(), nums.end());
+        int pivot = -1;
+        for(int i=nums.size()-2; i>=0; i--){
+            if(nums[i] < nums[i+1]){
+                pivot = i;
+                break;
+            }
+        } 
+        if(pivot == -1){ //maximum permutation given
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+
+        reverse(nums.begin() + pivot + 1, nums.end());
+
+
+        //swapping with just greater than pivot element
+        for(int i=pivot+1; i<nums.size(); i++){
+            if(nums[i] > nums[pivot]){
+                swap(nums[pivot], nums[i]);
+                break;
+            }
+        }
+        
     }
 };
