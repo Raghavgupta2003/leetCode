558060aa3fa6781c1f369d5f056571b9399059cc
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-06-21 02:10:40 +0530

    Time: 23 ms (7.54%), Space: 23 MB (12.67%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -1,32 +1,44 @@
 class Solution {
 public:
-    bool canEatWithinTime(long speed, vector<int> piles, int h){
-        long hours = 0;
-        for(int i=0; i<piles.size(); i++){
-            hours += ceil((double)piles[i]/speed); //to get uppper value we use ceil
-        }
-
-        if(hours <= h) return true;
-        return false;
-    }
     int minEatingSpeed(vector<int>& piles, int h) {
-        sort(piles.begin(), piles.end());
 
-        int low = 1;
-        int high = *max_element(piles.begin(), piles.end());
-        int ans = -1;
-        while(low <= high){
-            long mid = low + (high-low)/2;
-            if(canEatWithinTime(mid, piles, h)){
+        //----BRUTE FORCE with TLE--------
+        // int speed = 1;
+        // while(true){
+        //     int hours = 0;
+            
+        //     for(int i=0; i<piles.size(); i++){
+        //         hours += ceil((double)piles[i]/speed);
+        //     }
+
+        //     if(hours == h) return speed;
+        //     speed++;
+        // }
+        // return 0;
+
+        //----------BINARY SEARCH----------
+
+        long long low = 1;
+        long long high = *max_element(piles.begin(), piles.end());
+        long long ans = -1;
+        while(low<=high){
+            long long mid = low + (high-low)/2; //speed
+
+            long long hours = 0;
+            for(int i=0; i<piles.size(); i++){
+                hours += ceil((double)piles[i]/mid);
+            }
+
+            if(hours <= h){
+                //try less speed
                 ans = mid;
                 high = mid-1;
-            }else{
-                //we increase speed
+            }
+            else if(hours > h){
+                //try more speed
                 low = mid+1;
             }
         }
-
-
         return ans;
     }
 };
