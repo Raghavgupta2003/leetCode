ea7c0d8fa2c2080613f5d97d081cb67c9debe2bb
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-06-23 00:18:51 +0530

    Time: 0 ms (100%), Space: 65.1 MB (66.43%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
@@ -1,17 +1,36 @@
 class Solution {
 public:
     int numSubarrayProductLessThanK(vector<int>& nums, int k) {
-        int cnt = 0;
-        for(int i=0; i<nums.size(); i++){
-            int pdt = 1;
-            for(int j=i; j<nums.size(); j++){
-                pdt *= nums[j];
-                if(pdt >= k) break;
+    //---------------BRUTE FORCE---------------
+        // int cnt = 0;
+        // for(int i=0; i<nums.size(); i++){
+        //     int pdt = 1;
+        //     for(int j=i; j<nums.size(); j++){
+        //         pdt *= nums[j];
+        //         if(pdt >= k) break;
 
-                if(pdt < k){
-                    cnt++;
-                } 
+        //         if(pdt < k){
+        //             cnt++;
+        //         } 
+        //     }
+        // }
+
+        // return cnt;
+    
+    //----------OPTIMIZED--------------------
+        int n = nums.size();
+        int j = 0;
+        int pdt = 1;
+        int cnt = 0;
+        for(int i=0; i<n; i++){
+            pdt *= nums[i];
+
+            while(pdt >= k && j<=i){
+                pdt /= nums[j]; //reducing window size
+                j++;
             }
+
+            cnt += i-j+1; //number of subarrays ending at index j
         }
 
         return cnt;
