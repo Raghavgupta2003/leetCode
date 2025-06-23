809887d37643464da705514a4e766c89fe0c3fd6
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-06-23 10:09:30 +0530

    Time: 11 ms (33.43%), Space: 13.3 MB (36.4%) - LeetHub

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,7 +1,36 @@
 class Solution {
 public:
     int maxVowels(string s, int k) {
-        int n = s.size();
+        // int n = s.size();
+        // int windowLen = 0;
+
+        // int maxVowel = 0;
+        // int cntVowel = 0;
+        // int l = 0;
+        // int r = 0;
+        // while(r < n){
+        //     if(s[r] == 'a' || s[r] == 'e' || s[r] == 'i' || s[r] == 'o' || s[r] == 'u'){
+        //         cntVowel++;
+        //     }
+        //     windowLen++; //****length of window***
+
+        //     while(windowLen > k && l<=r){
+        //          if(s[l] == 'a' || s[l] == 'e' || s[l] == 'i' || s[l] == 'o' || s[l] == 'u') cntVowel--;
+
+        //          windowLen--;
+        //          l++;
+        //     }
+
+        //     maxVowel = max(maxVowel, cntVowel);
+        //     r++;
+        // }
+
+        // return maxVowel;
+
+        //---------------------------------------------------------
+        //since windowSize is fixed, we need not to take inner while loop
+
+          int n = s.size();
         int windowLen = 0;
 
         int maxVowel = 0;
@@ -12,17 +41,18 @@ public:
             if(s[r] == 'a' || s[r] == 'e' || s[r] == 'i' || s[r] == 'o' || s[r] == 'u'){
                 cntVowel++;
             }
-            windowLen++;
 
-            while(windowLen > k && l<=r){
+            if(r-l+1 == k){
+                maxVowel = max(maxVowel, cntVowel);
+
                  if(s[l] == 'a' || s[l] == 'e' || s[l] == 'i' || s[l] == 'o' || s[l] == 'u') cntVowel--;
-
-                 windowLen--;
+                 
                  l++;
+                 r++;
             }
-
-            maxVowel = max(maxVowel, cntVowel);
-            r++;
+            else{
+                r++;
+            }
         }
 
         return maxVowel;
