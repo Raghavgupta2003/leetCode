a28b2a32585ea13aa982f5e2d60d1e68f6a93ecf
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-06-23 09:40:08 +0530

    Time: 0 ms (100%), Space: 59.4 MB (25.75%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
@@ -1,24 +1,48 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-        //Can we understand this question as
-        //Longest SubArray containing 0 <= k
+        // // ***Can we understand this question as
+        // // Longest SubArray containing 0 <= k***
+        // int n = nums.size();
+        // int MaxLen = INT_MIN;
+        // int cnt0 = 0; 
+        // int j = 0;
+        // for(int i=0; i<n; i++){
+        //     if(nums[i] == 0){
+        //         cnt0++;
+        //     }
+
+        //     while(cnt0 > k && j<=i){
+        //         if(nums[j]==0)cnt0--;
+        //         j++;
+        //     }
+
+        //     if(cnt0 <= k) MaxLen = max(MaxLen, i-j+1);
+        // }
+        // return MaxLen;
+
+        //-----------------------------------------------
+
         int n = nums.size();
         int MaxLen = INT_MIN;
         int cnt0 = 0; 
+        int i=0;
         int j = 0;
-        for(int i=0; i<n; i++){
+        while(i<n){
             if(nums[i] == 0){
                 cnt0++;
             }
 
-            while(cnt0 > k && j<=i){
+            while(cnt0 > k){
                 if(nums[j]==0)cnt0--;
                 j++;
             }
 
-            if(cnt0 <= k) MaxLen = max(MaxLen, i-j+1);
+            MaxLen = max(MaxLen, i-j+1);
+            i++;
         }
         return MaxLen;
+
+
     }
 };
