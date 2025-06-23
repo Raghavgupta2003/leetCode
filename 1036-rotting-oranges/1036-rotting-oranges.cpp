e06bbb5b52b78a1bf23a211a70f0853e47653f6a
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-06-24 03:00:31 +0530

    Time: 0 ms (100%), Space: 16.9 MB (61.68%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,55 +1,54 @@
 class Solution {
 public:
+    vector<int> delrow = {0, 1, 0, -1};
+    vector<int> delcol = {1, 0, -1, 0};
     int orangesRotting(vector<vector<int>>& grid) {
-        // BFS will work
-        // as it work as level as miniute.
 
-        // first we make make a queue
-        // then insert rotten oranges in queue with time = 0;
+        queue<pair<pair<int,int>, int>> q;
 
-        int m = grid.size();
-        int n = grid[0].size();
-        queue<pair<pair<int, int>, int>> q; //{row, column, time}
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(grid[i][j] == 2){ //rotten oranges
-                    q.push({{i,j},0});
+        for(int i=0; i<grid.size(); i++){
+            for(int j=0; j<grid[0].size(); j++){
+                if(grid[i][j] == 2){ //rotten
+                    q.push({{i,j}, 0}); //0 means 0 minute
                 }
             }
         }
-        int tm = 0;
-        vector<int> delrow = {0, 1, 0, -1};
-        vector<int> delcol = {1, 0, -1, 0};
 
-        //BFS
+        int tmax = 0;
         while(q.size() > 0){
             int r = q.front().first.first;
             int c = q.front().first.second;
-            int t = q.front().second;
 
-            // updating tm
-            tm = max(tm, t);
+            int t = q.front().second;
+            tmax = max(tmax, t);
 
             q.pop();
 
             for(int i=0; i<4; i++){
-                int nrow = r + delrow[i];
-                int ncol = c + delcol[i];
+                int nr = r + delrow[i];
+                int nc = c + delcol[i];
 
-                if(nrow>=0 && ncol>=0 && nrow<m && ncol<n && grid[nrow][ncol] == 1){
-                    grid[nrow][ncol] = 2;
-                    q.push({{nrow, ncol}, t + 1});
+                int nt = t+1;
+
+                if(nr>=0 && nc>=0 && nr<grid.size() && nc<grid[0].size()){
+                    if(grid[nr][nc] == 1){//unrotten or unvisited 
+                        grid[nr][nc] = 2; //rotten it
+
+                        q.push({{nr, nc}, nt});
+                    }
                 }
             }
         }
 
-        //checking
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(grid[i][j] == 1) return -1;
+        for(int i=0; i<grid.size(); i++){
+            for(int j=0; j<grid[0].size(); j++){
+                if(grid[i][j] == 1){ //if every orange can't be rooten then return -1
+                    //still it is fresh
+                    return -1;
+                }
             }
         }
-
-        return tm;
+        return tmax;
+        
     }
 };
