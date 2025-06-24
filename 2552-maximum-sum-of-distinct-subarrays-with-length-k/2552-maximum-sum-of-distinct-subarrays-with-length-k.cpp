75460f9b6f6971a0fca60f31a7ce46b78c7351a8
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-06-24 11:29:27 +0530

    Time: 219 ms (10.15%), Space: 113.2 MB (5.07%) - LeetHub

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -1,90 +1,36 @@
-// class Solution {
-// public:
-//     long long maximumSubarraySum(vector<int>& nums, int k) {
-//         //Brute Force
-//         int max = 0;
-//         for(int i=0;i<nums.size()-k+1;i++){
-//             int sum = 0;
-//             unordered_set<int> s;
-//             for(int j=i; j<i+k; j++){
-//                 cout<<nums[j]<<" ";
-//                 s.insert(nums[j]);
-//                 sum+=nums[j];
-//             }
-//             cout<<endl;
-//             if(s.size()==k){
-//                 if(max<=sum) max=sum;
-//             }
-//         }
-
-//         return max;
-//     }
-// };
-
-//--------------------Approach 2-----------------------
-
-// class Solution {
-// public:
-//     long long maximumSubarraySum(vector<int>& nums, int k) {
-//         int max = 0;
-//         unordered_set<int> s; // set is used to check whether the elements are unique or not.
-//         int sum = 0;
-//         // making first subarray of size k
-//         for(int j=0; j<k; j++){
-//                 s.insert(nums[j]); 
-//                 sum+=nums[j];
-//         }
-//         if(s.size()==k){
-//             if(max<=sum) max=sum;
-//         }
-
-//         for(int i=k;i<nums.size();i++){
-//             s.erase(nums[i-k]);   //removing first element
-//             s.insert(nums[i]);   // adding [previous subarray.size()-1]+1 element
-//             sum += nums[i];
-//             sum -= nums[i-k];
-//             if(s.size()==k){
-//                 if(max<=sum) max=sum;
-//             }
-//         }
-
-//         return max;
-//     }
-// };
-
-
-
-//-----------------------------------------------------------
 class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
         int n = nums.size();
-        unordered_set<int> elements;
-        long long current_sum = 0;
-        long long max_sum = 0;
-        int begin = 0;
-        
-        for (int end = 0; end < n; end++) {
-            if (elements.find(nums[end]) == elements.end()) {
-                current_sum += nums[end];
-                elements.insert(nums[end]);
-                
-                if (end - begin + 1 == k) {
-                    max_sum = max(max_sum, current_sum);
-                    current_sum -= nums[begin];
-                    elements.erase(nums[begin]);
-                    begin++;
-                }
-            } else {
-                while (nums[begin] != nums[end]) {
-                    current_sum -= nums[begin];
-                    elements.erase(nums[begin]);
-                    begin++;
-                }
-                begin++;
+
+        int i=0;
+        int j=0;
+        long long currSum = 0;
+        long long MaxSum = 0;
+        set<int> s;
+
+        while(i<n){
+            // if condition voilates, start shrinking window
+            while(s.find(nums[i]) != s.end()){
+                currSum -= nums[j];
+                s.erase(nums[j]);
+                j++;
             }
+
+            currSum += nums[i];
+            s.insert(nums[i]);
+               
+            int window = (i-j+1);
+
+            if(window == k){
+                MaxSum = max(MaxSum , currSum);
+                s.erase(nums[j]);
+                currSum -= nums[j];
+                i++;
+                j++;
+            }
+            else i++;
         }
-        
-        return max_sum;
+        return MaxSum;
     }
 };
