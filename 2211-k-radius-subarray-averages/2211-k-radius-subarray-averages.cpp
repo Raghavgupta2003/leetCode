b9b069b390503e9738234a9d09dbe8beae980f64
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-06-24 09:41:35 +0530

    Time: 2 ms (81.59%), Space: 134.3 MB (68.84%) - LeetHub

--- 2211-k-radius-subarray-averages/2211-k-radius-subarray-averages.cpp
+++ 2211-k-radius-subarray-averages/2211-k-radius-subarray-averages.cpp
@@ -1,24 +1,63 @@
 class Solution {
 public:
     vector<int> getAverages(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<int> ans(n, -1);
-        vector<long long> prefix(n);
-        prefix[0] = nums[0];
-        for(int i=1; i<nums.size(); i++){
-            prefix[i] = prefix[i-1] + nums[i];
-        }
+
+    //-----------By prefixSum----------------
+        // int n = nums.size();
+        // vector<int> ans(n, -1);
+        // vector<long long> prefix(n);
+        // prefix[0] = nums[0];
+        // for(int i=1; i<nums.size(); i++){
+        //     prefix[i] = prefix[i-1] + nums[i];
+        // }
         
-        //we start from index k and end before n-k, as we know, we will not ger k element before it and after it
-        for(int i=k; i<n-k; i++){
-            long long sumTillNplusK = prefix[i+k];
-            long long SumbeforeNminusK = 0;
+        // //we start from index k and end before n-k, as we know, we will not ger k element before it and after it
+        // for(int i=k; i<n-k; i++){
+        //     long long sumTillNplusK = prefix[i+k];
+        //     long long SumbeforeNminusK = 0;
 
-            if(i-k != 0) SumbeforeNminusK = prefix[i-k-1];
-            ans[i] = (sumTillNplusK - SumbeforeNminusK)/(2*k + 1); //avg
-            //number = k(left) + k(right) + 1(ele itself)
+        //     if(i-k != 0) SumbeforeNminusK = prefix[i-k-1];
+        //     ans[i] = (sumTillNplusK - SumbeforeNminusK)/(2*k + 1); //avg
+        //     //number = k(left) + k(right) + 1(ele itself)
+        // }
+
+        // return ans;
+
+    //-----------------Sliding window----------------------
+
+        int n = nums.size();
+
+        vector<int> res(n, -1);
+         if (2 * k + 1 > n) return res;
+
+        //array filling start from "k" to "n-k"
+
+        int i=k;
+        //range left to right
+        int l = 0;
+        int r = i + k;
+
+        long long currSum = 0;
+        for(int i=l; i<=r; i++){
+            currSum += nums[i];
+        } 
+
+        res[i] = currSum/(2*k+1); //k left + k right + ith element itself
+        i++;
+
+        while(r+1 < n){
+            currSum -= nums[l];
+            currSum += nums[r+1];
+
+            l++;
+            r++;
+
+            res[i] = currSum/(2*k + 1);
+            i++;
         }
 
-        return ans;
+        return res;
+
     }
 };
+
