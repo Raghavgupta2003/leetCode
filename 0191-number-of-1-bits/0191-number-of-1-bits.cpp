1381edb3133e28853ad5a686d2447fab83754a32
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-06-26 09:39:36 +0530

    Time: 0 ms (100%), Space: 8.3 MB (11.29%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public:
     int hammingWeight(int n) {
-        int cnt = 0;
-        while(n){
-            if(n%2 == 1){ //set bit
-                cnt++;
-            }
-            n/=2;
+        // int cnt = 0;
+        // while(n){
+        //     if(n%2 == 1){ //set bit
+        //         cnt++;
+        //     }
+        //     n/=2;
+        // }
+        // return cnt;
+
+
+        //------------------------------------------
+
+        int countsets = 0;
+        for(int i=0; i<32; i++){
+            if((n & (1<<i)) != 0) countsets++;
         }
-        return cnt;
+        return countsets;
     }
 };
