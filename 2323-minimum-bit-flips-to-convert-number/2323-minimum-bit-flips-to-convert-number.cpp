050309a0abbc31d8b295647832511aa31caf0720
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-06-26 10:34:47 +0530

    Time: 0 ms (100%), Space: 7.8 MB (99.62%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
-   int binary(int num){
-        int bin = 0;
-        int cnt=0;
-        int count1=0;
-        while(num>0){
-            int temp = num%2;
-            if(temp==1) count1++;
-            // bin+= temp*(pow(10,cnt));
-            num/=2;
-            cnt++;
-        }
-        return count1++;
-   }
     int minBitFlips(int start, int goal) {
-        int ans = start ^ goal;
-        return binary(ans);
-    }
+        int x = start ^ goal;
 
+        //cnting set bit
+        int cnt = 0;
+        while(x){
+            if(x%2 == 1) cnt++;
+            x/=2;
+        }
+
+        return cnt;
+
+    }
 };
