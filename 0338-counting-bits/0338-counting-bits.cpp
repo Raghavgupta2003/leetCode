7d1028945867c355c4be94535fc043a8a8bb2222
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-06-26 10:03:20 +0530

    Time: 0 ms (100%), Space: 10.6 MB (80.36%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,20 +1,37 @@
+// class Solution {
+// public:
+//     int cntSet(int n){
+//         int cnt = 0;
+//         while(n){
+//             if((n&1) != 0) cnt++; //0th bit is set
+
+//             n = n>>1; //discard 0th bit
+//         }
+//         return cnt;
+//     }
+//     vector<int> countBits(int n) {
+//         vector<int> v;
+//         for(int i=0; i<n+1; i++){
+//             int x = cntSet(i);
+//             v.push_back(x);
+//         }
+//         return v;
+//     }
+// };
+
+
+
+//----------------------------------------
+
 class Solution {
 public:
-    int cntSet(int n){
-        int cnt = 0;
-        while(n){
-            if((n&1) != 0) cnt++; //0th bit is set
-
-            n = n>>1; //discard 0th bit
-        }
-        return cnt;
-    }
+    
     vector<int> countBits(int n) {
-        vector<int> v;
+        vector<int> arr(n+1, 0);
         for(int i=0; i<n+1; i++){
-            int x = cntSet(i);
-            v.push_back(x);
+            if((i%2) == 0) arr[i] = arr[i/2];
+            else arr[i] = arr[i/2] + 1;
         }
-        return v;
+        return arr;
     }
 };
