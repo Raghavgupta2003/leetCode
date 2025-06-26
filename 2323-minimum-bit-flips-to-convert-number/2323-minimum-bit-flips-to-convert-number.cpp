7ced4ed7cf46c6b707aa8410bd4f4b8ea3647df8
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-06-26 10:37:56 +0530

    Time: 0 ms (100%), Space: 7.9 MB (59.68%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
+        //we have to cnt, How many bits are not equal
+        //for this, best operator is XOR
+        //cnt set bits
+        //because bits are only set when different bits in XOR operation
+
+        //**thwew is not concept of minimum in this question**
         int x = start ^ goal;
 
         //cnting set bit
