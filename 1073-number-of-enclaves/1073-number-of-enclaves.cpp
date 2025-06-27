c702d6e5341954f49d3a9c9a9ea030ad12c45d6d
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-06-28 01:11:20 +0530

    Time: 18 ms (18.1%), Space: 37.8 MB (29.58%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.cpp
+++ 1073-number-of-enclaves/1073-number-of-enclaves.cpp
@@ -1,49 +1,52 @@
 class Solution {
 public:
-    vector<int> delrow = {1, 0, -1, 0};
-    vector<int> delcol = {0, 1, 0, -1};
-    void dfs(int i, int j, vector<vector<int>>& visited, vector<vector<int>>& grid){
+    vector<int> delrow = {0, 1, 0, -1};
+    vector<int> delcol = {1, 0, -1, 0};
+    void DFS(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& visited){
         visited[i][j] = 1;
 
         for(int k=0; k<4; k++){
-            int nrow = i+delrow[k];
-            int ncol = j+delcol[k];
+            int ir = i + delrow[k];
+            int ic = j + delcol[k];
 
-            if(nrow >=0 && ncol >=0 && nrow<grid.size() && ncol<grid[0].size()){
-                if(!visited[nrow][ncol] && grid[nrow][ncol] == 1)dfs(nrow, ncol, visited, grid);
+            if(ir >= 0 && ic >= 0 && ir < grid.size() && ic < grid[0].size()){
+                if(grid[ir][ic] == 1 && !visited[ir][ic]){
+                    DFS(ir, ic, grid, visited);
+                }
             }
         }
     }
     int numEnclaves(vector<vector<int>>& grid) {
-        //we apply dfs on boundry
-        //if we find 1 on boundry apply dfs on them and mark them visited
-        //so, we cannot move out of grid by that land
-        //now count unvisited l in the array its our ans.
+        //APPLY DFS ON BOUNDARIES WHERE LAND FOUND AND MARK AS VISITED 
+        //NOW, COUNT THE NUMBER OF CELLS HAVING LAND(1) which aare not visited
+
         int m = grid.size();
         int n = grid[0].size();
         vector<vector<int>> visited(m, vector<int>(n, 0));
-        //boudry dfs column
+
         for(int i=0; i<m; i++){
-            if(grid[i][0] == 1) dfs(i, 0, visited, grid);
+            if(grid[i][0] == 1) DFS(i, 0, grid, visited);
 
-            if(grid[i][n-1] == 1) dfs(i, n-1, visited, grid);
+            if(grid[i][n-1] == 1) DFS(i, n-1, grid, visited);
         }
-        //boudry dfs row
+
         for(int j=0; j<n; j++){
-            if(grid[0][j] == 1) dfs(0, j, visited, grid);
+            if(grid[0][j] == 1) DFS(0, j, grid, visited);
 
-            if(grid[m-1][j] == 1) dfs(m-1, j, visited, grid);
+            if(grid[m-1][j] == 1) DFS(m-1, j, grid, visited);
         }
-
-        //count unvisited 1
+        
+        //counting unvisited land
         int cnt = 0;
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(visited[i][j] == 0 && grid[i][j] == 1){
-                    cnt++;
+                if(!visited[i][j]){
+                    if(grid[i][j] == 1) cnt++;
                 }
             }
         }
+
         return cnt;
+
     }
 };
