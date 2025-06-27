b5139295c3a087c5a6a8e61fb3829acf45fbb88d
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-06-28 00:50:02 +0530

    Time: 0 ms (100%), Space: 13.9 MB (91.66%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,63 +1,55 @@
 class Solution {
 public:
-    vector<int> delrow = {0, 1, 0, -1};
-    vector<int> delcol = {1, 0, -1, 0};
-    // void dfs(int i, int j, vector<vector<char>>& grid, vector<vector<int>>& visited){
-    //     // if(i<0 || j<0 || i>=board.size() || j>=board[0].size()) return;
-    //     if(grid[i][j] == 'X') return;
+    vector<int> delrow = {1, 0, -1, 0};
+    vector<int> delcol = {0, 1, 0, -1};
 
-    //     visited[i][j] = 1;
-
-    //     for(int k=0; k<4; k++){
-    //         int nrow = i + delrow[k];
-    //         int ncol = j + delcol[k];
-            
-    //         if(nrow >=0 && ncol>=0 && nrow<grid.size() && ncol<grid[0].size()){
-    //             if(!visited[nrow][ncol]) dfs(nrow, ncol, grid, visited);
-                
-    //         }
-    //     }
-    // }
-
-    void dfs(int i, int j, vector<vector<char>>& grid, vector<vector<int>>& visited){
-        // if(i<0 || j<0 || i>=board.size() || j>=board[0].size()) return;
-
-        visited[i][j] = 1;
+    void DFS(int i, int j, vector<vector<char>>& board){
+        board[i][j] = 'Y';
 
         for(int k=0; k<4; k++){
-            int nrow = i + delrow[k];
-            int ncol = j + delcol[k];
-            
-            if(nrow >=0 && ncol>=0 && nrow<grid.size() && ncol<grid[0].size()){
-                if(!visited[nrow][ncol] && grid[i][j] == 'O') dfs(nrow, ncol, grid, visited);
+            int ni = i + delrow[k];
+            int nj = j + delcol[k];
+
+            if(ni >= 0 && nj >= 0 && ni < board.size()  && nj < board[0].size()){
+                if(board[ni][nj] == 'O'){
+                    DFS(ni, nj, board);
+                }
             }
         }
     }
     void solve(vector<vector<char>>& board) {
+        //question is something like we cannot move from "o regin" to out of the grid
+        //so, in this question i will do  DFS traversal on boundaryies where "o" founded
+        //and mark them as "y"
         int m = board.size();
         int n = board[0].size();
 
-        vector<vector<int>> visited(m, vector<int>(n, 0));
-        
-        //whenever we find 'O' on boundry we apply dfs on it
-        //DFS mark the all connected 'O' with boundry 'O' in visited matrix
-        //we do not have to convet visited 'O' to 'X'
-        for(int j=0; j<n; j++){
-            if(board[0][j] == 'O' ) dfs(0, j, board, visited); //upper boundry row
-            if(board[m-1][j] == 'O' ) dfs(m-1, j, board, visited); //lower boundry row
+        for(int i=0; i<m; i++){
+            if(board[i][0] == 'O') DFS(i, 0, board);
+
+            if(board[i][n-1] == 'O') DFS(i, n-1, board);
         }
 
+         for(int j=0; j<n; j++){
+            if(board[0][j] == 'O') DFS(0, j, board);
+
+            if(board[m-1][j] == 'O') DFS(m-1, j, board);
+        }
+
+        //Now mark all "o" (present in middle, whose no path to out of board) as "x"
+        //mark again "y" as "o";
+
         for(int i=0; i<m; i++){
-            if(board[i][0] == 'O' ) dfs(i, 0, board, visited);
-            if(board[i][n-1] == 'O' ) dfs(i, n-1, board, visited);
+            for(int j=0; j<n; j++){
+                if(board[i][j] == 'O') board[i][j] = 'X';
+            }
         }
 
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(!visited[i][j]){
-                    if(board[i][j] == 'O') board[i][j] = 'X';
-                }
+                if(board[i][j] == 'Y') board[i][j] = 'O';
             }
         }
+
     }
 };
