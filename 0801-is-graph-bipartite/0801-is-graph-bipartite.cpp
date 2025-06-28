562f3993d3a6e3d28fad8f71d074ce4c6072e3b0
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-06-28 18:59:32 +0530

    Time: 0 ms (100%), Space: 17.5 MB (35.88%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -1,75 +1,42 @@
-// class Solution {
-// public:
-//     bool dfs(int node, vector<int>& visited, int color, vector<vector<int>>& graph){
-//         visited[node] = color;
-
-//         for(auto it : graph[node]){
-//             if(visited[it] == -1){ //not visited
-//                 if(!dfs(it, visited, !color, graph)) return false;
-//             }
-//             else{
-//                 // already visited
-//                 // check if it has same color..?
-//                 // if same color NOT a bipartite which means return false;
-//                 if(visited[it] == color){
-//                     return false;
-//                 }
-//             }
-//         }
-
-//         return true;
-//     }
-//     bool isBipartite(vector<vector<int>>& graph) {
-//         int n = graph.size(); //number of nodes
-
-//         //in visited array we store color as 0 and 1 
-//         vector<int> visited(n, -1);
-//         int color = 0;
-//         for(int i=0; i<n; i++){
-//             if(visited[i] == -1){ //not visited 
-//                 if(!dfs(i, visited, color, graph)) return false;
-//             }
-//         }
-
-//         return true;
-//     }
-// };
-
-//-----------------------------BFS--------------------------------------
 class Solution {
 public:
-    bool BFS(int node, vector<int>& visited, vector<vector<int>>& graph){
-        queue<int> q;
-        q.push(node);
-        visited[node] = 0; //0th is colored with 0;
-
-        while(q.size() > 0){
-            int node = q.front();
-            q.pop();
-
-            for(auto it : graph[node]){
-                if(visited[it] == -1){
-                    q.push(it); 
-                    visited[it] = !visited[node];//coloring with opposite color of node
-                }else{
-                    //already colored, visited
-                    if(visited[it] == visited[node]) return false;
-                }
-            }
-        }
-
-        return true;
-
-    }
     bool isBipartite(vector<vector<int>>& graph) {
-        int n = graph.size(); //number of nodes
+        // its something like we want to color the graph with 2 colors, but no two adjancent nodes should have same color
+        // we assume 2 colors be 0, 1
+        // unclored be -1
+        //if we get alreadycolred adjacent with samecolr return false;
+        //else return true
 
-        //in visited array we store color as 0 and 1 
-        vector<int> visited(n, -1);
+        //*************this question easily solved by BFS as well as DFS****
 
-        for(int i=0; i<n; i++){
-            if(visited[i] == -1){
-                if(!BFS(i, visited, graph)) return false;
+        //connected components
+        
+        int n = graph.size();
+        vector<int> color(n, -1); //visited arr
+
+
+        for(int i=0; i<n; i++){//for connected components
+            if(color[i] == -1){//unvisited then apply BFS
+                //BFS
+                queue<int> q;
+                q.push(i); 
+                color[i] = 0; //node 0 is colored with 0 color
+                while(q.size() > 0){
+                    int node = q.front();
+
+                    q.pop();
+
+                    for(auto it: graph[node]){
+                        if(color[it] == -1){ //not colored or not visited
+                            color[it] = !color[node]; //coloring with diff color
+                            q.push(it);
+                        }else{
+                            //already colored
+
+                            if(color[it] == color[node]) return false;
+                        }
+                    }
+                }
             }
         }
         return true;
