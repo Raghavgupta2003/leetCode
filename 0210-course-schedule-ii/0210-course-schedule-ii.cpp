3479dd59b9d90a0381e0b815a57fa33218f94dca
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-06-29 00:58:37 +0530

    Time: 0 ms (100%), Space: 18 MB (78.57%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,47 +1,37 @@
 class Solution {
 public:
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        // this question  is somewhat similar to toposort
-        // or cycle detection by toposort
-        // we are given eith reverse edges
-        // we will correct the order of edges
+    vector<int> findOrder(int numCourses, vector<vector<int>>& edges) {
+        //{u, v}
+        //since we have to complete course v before u
+        //adj[v].push_back(u);
 
-        for(auto it : prerequisites){
-            reverse(it.begin(), it.end());
-        }
-
-        // creating adj list
         int n = numCourses; //number of nodes
+
+        vector<int> indegree(n, 0);
+
         vector<vector<int>> adj(n, vector<int>(0));
-        for(int i=0; i<prerequisites.size(); i++){
-            int a = prerequisites[i][0];
-            int b = prerequisites[i][1];
-            adj[a].push_back(b);
+        for(int i=0; i<edges.size(); i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+
+            adj[v].push_back(u);
+            indegree[u]++;
         }
 
-        //Working with toposort
-
-        //indegree
-        vector<int> indegree(n);
-        for(int i=0; i<n; i++){
-            for(auto it : adj[i]){
-                indegree[it]++;
-            }
-        }
-
-        //initialize queue
-        //insert nodes whose indegree is 0
+        //khans Algorithm
 
         queue<int> q;
-        for(int i=0; i<indegree.size(); i++){
-            if(indegree[i] == 0) q.push(i);
+        for(int i=0; i<indegree.size(); i++){\
+            // cout<<indegree[i]<<" ";
+            if(indegree[i] == 0) q.push(i); //pushing node whose indegree is zero
         }
-
-        //Applying BFS
+        
         vector<int> toposort;
+    
         while(q.size() > 0){
             int node = q.front();
             q.pop();
+
             toposort.push_back(node);
 
             for(auto it : adj[node]){
@@ -50,11 +40,8 @@ public:
             }
         }
 
-        //returning 
-        reverse(toposort.begin(), toposort.end());
-        if(toposort.size() == n) return toposort; //NO cycle dependencies found
-                                              // we are able to complete course
+        if(toposort.size() == n) return toposort;
 
-        return {}; // cyclic dependencies found;
+        return {};
     }
 };
