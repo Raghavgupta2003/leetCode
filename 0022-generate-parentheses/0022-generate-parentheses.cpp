2b32aa574ecd873b57daf77ae2fcd83f7b86710c
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-07-01 10:50:45 +0530

    Time: 8 ms (9.71%), Space: 21 MB (7.73%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,34 +1,64 @@
+// class Solution {
+// public:
+//     bool valid(string str){
+//         int cnt = 0;
+//         for(int i=0; i<str.size(); i++){
+//             if(str[i] == '(') cnt++;
+//             else{
+//                 cnt--;
+//                 if(cnt < 0) return false;  //opening brackets less before closing bracket
+//             }
+//         }
+
+//         return cnt == 0;
+//     }
+//     void help(int n, vector<string>& ans, string str){
+//         if(str.size() == 2*n){
+//             if(valid(str)) ans.push_back(str);
+//             return;
+//         }
+
+//         // "(" pick
+//         help(n, ans, str+"(");
+
+//         //")" pick
+//         help(n, ans, str+")");
+
+//     }
+//     vector<string> generateParenthesis(int n) {
+//         string str = "";
+//         vector<string> ans;
+//         help(n, ans, str);
+//         return ans;
+//     }
+// };
+
+
+//-----------------------------------------------------
+
 class Solution {
 public:
-    bool valid(string str){
-        int cnt = 0;
-        for(int i=0; i<str.size(); i++){
-            if(str[i] == '(') cnt++;
-            else{
-                cnt--;
-                if(cnt < 0) return false;  //opening brackets less before closing bracket
-            }
-        }
-
-        return cnt == 0;
-    }
-    void help(int n, vector<string>& ans, string str){
+    void help(int n, vector<string>& ans, string str, int open, int close){
         if(str.size() == 2*n){
-            if(valid(str)) ans.push_back(str);
+            if(open == close) ans.push_back(str);
             return;
         }
 
         // "(" pick
-        help(n, ans, str+"(");
+        if(open >= close)
+        help(n, ans, str+"(", open+1, close);
 
         //")" pick
-        help(n, ans, str+")");
+        if(open > close)
+        help(n, ans, str+")", open, close+1);
 
     }
     vector<string> generateParenthesis(int n) {
         string str = "";
         vector<string> ans;
-        help(n, ans, str);
+        int open = 0;
+        int close = 0;
+        help(n, ans, str, open, close);
         return ans;
     }
 };
