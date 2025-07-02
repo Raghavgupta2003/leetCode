05f6db80f408dd2756b07de9accff261a6df5d20
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-07-02 09:25:21 +0530

    Time: 0 ms (100%), Space: 10.4 MB (85.46%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,29 +1,41 @@
 class Solution {
 public:
-    void helper(int i, set<vector<int>>& ans, vector<int>& help, vector<int>& nums){
-        if(i>= nums.size()){
-            ans.insert(help);
-            return;
-        }
+    // void helper(int i, set<vector<int>>& ans, vector<int>& help, vector<int>& nums){
+    //     if(i>= nums.size()){
+    //         ans.insert(help);
+    //         return;
+    //     }
+        
+    //     //pick
+    //     help.push_back(nums[i]);
+    //     helper(i+1, ans, help, nums);
+    //     help.pop_back();
+
+    //     //not pick
+    //     helper(i+1, ans, help, nums);
+    // }
+
+     void helper(int i, vector<vector<int>>& ans, vector<int>& help, vector<int>& nums){
+            
+        ans.push_back(help);
+       
         
         //pick
-        help.push_back(nums[i]);
-        helper(i+1, ans, help, nums);
-        help.pop_back();
+        for(int index = i; index<nums.size(); index++){
+            if(index > i && nums[index] == nums[index-1]) continue;
 
-        //not pick
-        helper(i+1, ans, help, nums);
+            help.push_back(nums[index]);
+            helper(index+1, ans, help, nums);
+            help.pop_back();
+        }
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        set<vector<int>> ans;
+        vector<vector<int>> ans;
         vector<int> help;
         helper(0, ans, help, nums);
-
-        vector<vector<int>> arr;
-        arr.assign(ans.begin(),ans.end());
-
-        sort(arr.begin(), arr.end());
-        return arr;
+       
+        // sort(ans.begin(), ans.end());
+        return ans;
     }
 };
