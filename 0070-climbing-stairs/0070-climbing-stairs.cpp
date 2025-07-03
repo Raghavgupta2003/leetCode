c20fc4b463920089d8f7ac2b9e1a44c8d27ffda4
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-07-03 10:43:04 +0530

    Time: 0 ms (100%), Space: 8.5 MB (51.69%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -13,14 +13,29 @@ public:
 
     //-----------------------------------
 
+    // int climbStairs(int n) {
+    //     vector<int> dp(n+1, -1);
+
+    //     dp[0] = 1;
+    //     dp[1] = 1;
+    //     for(int i=2; i<dp.size(); i++){
+    //         dp[i] = dp[i-1]+dp[i-2];
+    //     }
+    //     return dp[n];
+    // }
+
+    //-------------------------------------------
+
     int climbStairs(int n) {
         vector<int> dp(n+1, -1);
 
-        dp[0] = 1;
-        dp[1] = 1;
+        int prev = 1;
+        int curr = 1;
         for(int i=2; i<dp.size(); i++){
-            dp[i] = dp[i-1]+dp[i-2];
+            int temp = prev+curr;
+            prev = curr;
+            curr = temp;
         }
-        return dp[n];
+        return curr;
     }
 };
