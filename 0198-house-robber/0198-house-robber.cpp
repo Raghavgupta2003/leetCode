a1a156b43336f04e00c2a96c94dda3a2e24ad025
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-07-03 11:07:11 +0530

    Time: 2 ms (7.45%), Space: 10.7 MB (22.3%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,64 +1,18 @@
-// class Solution {
-// public:
-//     int helper(int idx, vector<int> &nums){
-//         if(idx>=nums.size()) return 0;
-//         //pick 
-        
-//         int left = nums[idx] + helper(idx+2, nums);
-//         //not pick
-//         int right = 0 + helper(idx + 1, nums);
-
-//         return max(left,right);
-//     }
-//     int rob(vector<int>& nums) {
-//         return helper(0, nums);
-//     }
-// };
-
-//---------------Memorization-------------------
-
-
-// class Solution {
-// public:
-//     int helper(int idx, vector<int> &nums, vector<int> &dp){
-//         if(idx>=nums.size()) return 0;
-
-//         if(dp[idx]!=-1) return dp[idx];
-//         //pick 
-//         int left = nums[idx] + helper(idx+2, nums,dp);
-//         //not pick
-//         int right = 0 + helper(idx + 1, nums,dp);
-
-//         return dp[idx] = max(left,right);
-//     }
-//     int rob(vector<int>& nums) {
-//         int n = nums.size();
-//         vector<int> dp(n,-1);
-//         return helper(0, nums,dp);
-//     }
-// };
-
-//---------------Tabulation-------------------
-
-
 class Solution {
 public:
+    int rec(int n, vector<int>& nums, vector<int>& dp){
+        if(n < 0) return 0;
+
+        if(dp[n]!=-1) return dp[n];
+        int pick = nums[n] + rec(n-2, nums, dp);
+        int notpick = 0 + rec(n-1, nums, dp);
+
+        return  dp[n] = max(pick, notpick);
+    }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n,-1);
+        vector<int> dp(n, -1);
 
-        dp[0] = nums[0];
-        if(nums.size() > 1) dp[1] =  max(nums[0],nums[1]);
-        
-        for(int i=2; i<dp.size(); i++){
-            //pick
-            int left = nums[i] + dp[i-2];
-            //not pick
-            int right = dp[i-1];
-
-            dp[i] = max(left,right);
-        }
-
-        return dp[n-1];
+        return rec(n-1,nums, dp);
     }
 };
