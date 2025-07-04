89ea4e54c058456e6a1d360d26157cb49de053fb
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-07-05 03:02:40 +0530

    Time: 497 ms (45.29%), Space: 11.7 MB (24.54%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    vector<int> delrow = {1, 0, -1, 0};
-    vector<int> delcol = {0, 1, 0, -1};
-    bool dfs(int i, int j, int k, vector<vector<char>>& board, string word){
-        if(board[i][j] != word[k]) return false;
-        if(k == word.size()-1) return true;
-        
-
-        char temp = board[i][j];
+    vector<int> delrow = {0, 1, 0, -1};
+    vector<int> delcol = {1, 0, -1, 0};
+    bool dfs(int i, int j, vector<vector<char>>& board, int k, string word){
+        if(word[k] != board[i][j]) return false;
+        if(word.size() == k+1) return true;
+    
+        int temp = board[i][j];
         board[i][j] = '#';
-        for(int m=0; m<4; m++){
-            int nr = i+delrow[m];
-            int nc = j+delcol[m];
 
-            if(nr>=0 && nc>=0 && nr<board.size() && nc < board[0].size()){
-                if(board[nr][nc] != '#'){
-                    if(dfs(nr, nc, k+1, board, word)) return true;
+        for(int p=0; p<4; p++){
+            int r = i+delrow[p];
+            int c = j+delcol[p];
+
+            if(r>=0 && c>=0 && r<board.size() && c<board[0].size()){
+                if(board[r][c]!='#'){
+                    if(dfs(r, c, board, k+1, word)) return true;
                 }
             }
         }
+
         board[i][j] = temp;
         return false;
     }
@@ -29,8 +30,7 @@ public:
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(board[i][j] == word[0]){
-                    string str = "";
-                    if(dfs(i, j, 0, board, word)) return true;
+                    if(dfs(i, j, board, 0, word)) return true;
                 }
             }
         }
