5f547b172a1ff85b71a3940b9feb9fca36a8a9fc
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-07-08 09:38:30 +0530

    Time: 7 ms (56.96%), Space: 16.7 MB (35.83%) - LeetHub

--- 1105-uncrossed-lines/1105-uncrossed-lines.cpp
+++ 1105-uncrossed-lines/1105-uncrossed-lines.cpp
@@ -1,19 +1,53 @@
+// class Solution {
+// public:
+//     int lcs(int n1, int n2, vector<int>& nums1, vector<int>& nums2, vector<vector<int>> &dp){
+//         if(n1 < 0 || n2 < 0) return 0; //anyone index out of bound
+
+//         if(dp[n1][n2] != -1) return dp[n1][n2];
+
+//         if(nums1[n1] == nums2[n2]) return 1+lcs(n1-1, n2-1, nums1, nums2, dp);
+
+//         return dp[n1][n2] = max(lcs(n1-1, n2, nums1, nums2, dp), lcs(n1, n2-1, nums1, nums2, dp));
+//     }
+//     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
+//         int n1 = nums1.size();
+//         int n2 = nums2.size();
+
+//         vector<vector<int>> dp(n1, vector<int>(n2, -1));
+//         return lcs(n1-1, n2-1, nums1, nums2, dp);
+//     }
+// };
+
+//-------------------------------------------------------------------------------
+
 class Solution {
 public:
-    int lcs(int n1, int n2, vector<int>& nums1, vector<int>& nums2, vector<vector<int>> &dp){
-        if(n1 < 0 || n2 < 0) return 0;
-
-        if(dp[n1][n2] != -1) return dp[n1][n2];
-
-        if(nums1[n1] == nums2[n2]) return 1+lcs(n1-1, n2-1, nums1, nums2, dp);
-
-        return dp[n1][n2] = max(lcs(n1-1, n2, nums1, nums2, dp), lcs(n1, n2-1, nums1, nums2, dp));
-    }
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
         int n1 = nums1.size();
         int n2 = nums2.size();
 
-        vector<vector<int>> dp(n1, vector<int>(n2, -1));
-        return lcs(n1-1, n2-1, nums1, nums2, dp);
+        vector<vector<int>> dp(n1+1, vector<int>(n2+1, 0));
+        //basecase
+        // when i=0 or j=0 then return 0
+
+        // for(int j=0; j<=n2; j++){
+        //     dp[0][j] = 0;
+        // }
+
+        // for(int i=0; i<=n1; i++){
+        //     dp[i][0] = 0;
+        // }
+
+        for(int i=1; i<=n1; i++){
+            for(int j=1; j<=n2; j++){
+                if(nums1[i-1]==nums2[j-1]){
+                    dp[i][j] = 1+dp[i-1][j-1];
+                } 
+                else{
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n1][n2];
     }
 };
