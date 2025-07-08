936c7431efd1ac87cc691f9096152b1b6a8c6f00
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-07-08 11:10:49 +0530

    Time: 91 ms (38.41%), Space: 76 MB (43.03%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -27,38 +27,125 @@
 //----------------------------tabulation----------------------
 
 
+// class Solution {
+// public:
+//     int longestPalindromeSubseq(string s) {
+//         string s1 = s;
+//         string s2 = s;
+//         reverse(s2.begin(), s2.end());
+
+//         int n1 = s1.size();
+//         int n2 = s2.size();
+
+//         vector<vector<int>> dp(n1+1, vector<int>(n2+1, 0)); //we have taken +1 size
+
+//         //basecase -> if(i==0 || j==0) return 0;
+
+//         for(int i=0; i<=n1; i++){
+//             dp[i][0]=0;
+//         }
+//         for(int j=0; j<=n2; j++){
+//             dp[0][j] = 0;
+//         }
+
+//         for(int i=1; i<=n1; i++){
+//             for(int j=1; j<=n2; j++){
+//                 if(s1[i-1] == s2[j-1]){
+//                     dp[i][j] = 1+ dp[i-1][j-1];
+//                 }
+//                 else{
+//                     dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+//                 }
+//             }
+//         }
+
+//         return dp[n1][n2];
+//     }
+// };
+
+
+//-----------------------by single string only-------------------------
+
+// class Solution {
+// public:
+//     int lcs(int n1, int n2, string s, vector<vector<int>>& dp){
+//         if(n1<0 || n2>=s.size()) return 0;
+    
+//         if(dp[n1][n2] != -1) return dp[n1][n2];
+
+//         if(s[n1] == s[n2]) return 1+lcs(n1-1, n2+1, s, dp);
+
+//         return dp[n1][n2] = max(lcs(n1-1, n2, s, dp), lcs(n1, n2+1, s, dp));
+//     }
+//     int longestPalindromeSubseq(string s) {
+//         int n = s.size();
+
+//         vector<vector<int>> dp(n, vector<int>(n, -1));
+//         int n1 = n-1;
+//         int n2 = 0;
+//         return lcs(n1, n2, s, dp);
+//     }
+// };
+
+
+//-----------------by single string but we travel only half of array-------
+
+// class Solution {
+// public:
+//     int lcs(int n1, int n2, string s, vector<vector<int>>& dp){
+//         // base case
+//         if(n1 < n2) return 0;
+//         if(n1 == n2) return 1; //part of palindrome
+       
+        
+//         if(dp[n1][n2] != -1) return dp[n1][n2];
+
+//         if(s[n1] == s[n2]) return 2+lcs(n1-1, n2+1, s, dp);
+
+//         return dp[n1][n2] = max(lcs(n1-1, n2, s, dp), lcs(n1, n2+1, s, dp));
+//     }
+//     int longestPalindromeSubseq(string s) {
+//         int n = s.size();
+
+//         vector<vector<int>> dp(n, vector<int>(n, -1));
+//         int n1 = n-1;
+//         int n2 = 0;
+//         return lcs(n1, n2, s, dp);
+//     }
+// };
+
+
+//----------------tabulation---------------------------
+
 class Solution {
 public:
     int longestPalindromeSubseq(string s) {
-        string s1 = s;
-        string s2 = s;
-        reverse(s2.begin(), s2.end());
+        int n = s.size();
 
-        int n1 = s1.size();
-        int n2 = s2.size();
-
-        vector<vector<int>> dp(n1+1, vector<int>(n2+1, 0)); //we have taken +1 size
-
-        //basecase -> if(i==0 || j==0) return 0;
-
-        for(int i=0; i<=n1; i++){
-            dp[i][0]=0;
-        }
-        for(int j=0; j<=n2; j++){
-            dp[0][j] = 0;
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+       
+        //basecase : fill all zero for(i < j)
+        //         : fill 1 at all (i==j)
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(i==j) dp[i][j] = 1;
+                if(i<j) dp[i][j] = 0;
+            }
         }
 
-        for(int i=1; i<=n1; i++){
-            for(int j=1; j<=n2; j++){
-                if(s1[i-1] == s2[j-1]){
-                    dp[i][j] = 1+ dp[i-1][j-1];
+        //llogic
+        // dp.size()-2 == n-1
+        for(int i=1; i<dp.size(); i++){
+            for(int j=dp.size()-2; j>=0; j--){
+                if(s[i-1]==s[j]){
+                    dp[i][j] = 1 + dp[i-1][j+1];
                 }
                 else{
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                    dp[i][j] = max(dp[i-1][j], dp[i][j+1]);
                 }
             }
         }
 
-        return dp[n1][n2];
+        return dp[n][0];
     }
 };
