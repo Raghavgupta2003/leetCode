a0c2d0576aad4a2f19222b4cab54a6f368a47f49
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-07-08 10:33:45 +0530

    Time: 99 ms (10.18%), Space: 75.9 MB (62.63%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,28 +1,30 @@
 // class Solution {
 // public:
-//     int helper(int idx1, int idx2, string s1, string s2, vector<vector<int>>& dp){
-//         if(idx1<0 || idx2<0) return 0;
+//     int lcs(int n1, int n2, string s1, string s2, vector<vector<int>>& dp){
+//         if(n1<0 || n2<0) return 0;
         
-//         if(dp[idx1][idx2] != -1) return dp[idx1][idx2];
+//         if(dp[n1][n2] != -1) return dp[n1][n2];
 
-//         if(s1[idx1] == s2[idx2]) return dp[idx1][idx2] =  1 + helper(idx1-1, idx2-1, s1, s2, dp);
-        
-//         return dp[idx1][idx2] = 0 + max(helper(idx1-1, idx2, s1, s2, dp), helper(idx1, idx2-1, s1, s2, dp));
+//         if(s1[n1] == s2[n2]) return 1+lcs(n1-1, n2-1, s1, s2, dp);
+
+//         return dp[n1][n2] = max(lcs(n1-1, n2, s1, s2, dp), lcs(n1, n2-1, s1, s2, dp));
 //     }
 //     int longestPalindromeSubseq(string s) {
 //         string s1 = s;
 //         string s2 = s;
 //         reverse(s2.begin(), s2.end());
 
-//         int n = s.size();
-//         vector<vector<int>> dp(n, vector<int> (n, -1));
+//         int n1 = s1.size();
+//         int n2 = s2.size();
 
-//         return helper(n-1, n-1, s1, s2, dp);
+//         vector<vector<int>> dp(n1, vector<int>(n2, -1));
+
+//         return lcs(n1-1, n2-1, s1, s2, dp);
 //     }
 // };
 
 
-//-------------------------------------------------------------------
+//----------------------------tabulation----------------------
 
 
 class Solution {
@@ -32,27 +34,31 @@ public:
         string s2 = s;
         reverse(s2.begin(), s2.end());
 
-        int n = s.size();
-        vector<vector<int>> dp(n+1, vector<int> (n+1, 0));
-        
-        //base case
-        for(int i=0; i<=n; i++){
-            dp[i][0] = 0;
-        }  
-        for(int j=0; j<=n; j++){
-            dp[0][j] = 0;
-        }    
-        //dp
+        int n1 = s1.size();
+        int n2 = s2.size();
 
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=n; j++){
+        vector<vector<int>> dp(n1+1, vector<int>(n2+1, 0)); //we have taken +1 size
+
+        //basecase -> if(i==0 || j==0) return 0;
+
+        for(int i=0; i<=n1; i++){
+            dp[i][0]=0;
+        }
+        for(int j=0; j<=n2; j++){
+            dp[0][j] = 0;
+        }
+
+        for(int i=1; i<=n1; i++){
+            for(int j=1; j<=n2; j++){
                 if(s1[i-1] == s2[j-1]){
-                    dp[i][j] = 1 + dp[i-1][j-1];
+                    dp[i][j] = 1+ dp[i-1][j-1];
                 }
-                else dp[i][j] = 0 + max(dp[i-1][j], dp[i][j-1]);
+                else{
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
             }
         }
 
-        return dp[n][n];    
+        return dp[n1][n2];
     }
 };
