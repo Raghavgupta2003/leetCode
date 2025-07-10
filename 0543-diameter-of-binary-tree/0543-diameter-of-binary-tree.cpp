6192ba99718a1f717d3bf2c1d43b7bc71717f786
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-07-11 00:10:45 +0530

    Time: 0 ms (100%), Space: 23.9 MB (19.38%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -9,11 +9,47 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     //BRUTE FORCE:- travel in tree
+//     //for each node calculate leftheight + rightheight
+//     //compare it with maximum
+//     //return maximum
+//     int maxDia = INT_MIN;
+//     int height(TreeNode* root){
+//         if(root == NULL) return 0;
+
+//         int left = height(root->left);
+//         int right = height(root->right);
+
+//         return 1 + max(left, right);
+//     }
+
+//     void diameter(TreeNode* root){
+//         if(root == NULL) return;
+
+//         int leftheight = height(root->left);
+//         int rightheight = height(root->right);
+
+//         maxDia = max(maxDia, leftheight + rightheight);
+
+//         diameter(root->left);
+//         diameter(root->right);
+//     }
+//     int diameterOfBinaryTree(TreeNode* root) {
+//         diameter(root);
+//         return maxDia;
+//     }
+// };
+
+
+//----------------------------------------------------------------------
+
+
 class Solution {
 public:
-    //BRUTE FORCE:- travel in tree
-    //for each node calculate leftheight + rightheight
-    //compare it with maximum
+    //OPTIMAL: in the height code itself
+    //we store maximum value (leftheight + rightheight)
     //return maximum
     int maxDia = INT_MIN;
     int height(TreeNode* root){
@@ -22,22 +58,12 @@ public:
         int left = height(root->left);
         int right = height(root->right);
 
+        maxDia = max(maxDia, left + right);
+
         return 1 + max(left, right);
     }
-
-    void diameter(TreeNode* root){
-        if(root == NULL) return;
-
-        int leftheight = height(root->left);
-        int rightheight = height(root->right);
-
-        maxDia = max(maxDia, leftheight + rightheight);
-
-        diameter(root->left);
-        diameter(root->right);
-    }
     int diameterOfBinaryTree(TreeNode* root) {
-        diameter(root);
+        height(root);
         return maxDia;
     }
 };
