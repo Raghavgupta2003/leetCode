6efc01fa4ce8bb7b5379aea76938728374d89484
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-07-10 23:52:38 +0530

    Time: 0 ms (100%), Space: 23.1 MB (56.36%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -56,9 +56,10 @@ public:
 
         int left = height(root->left);
         int right = height(root->right);
-
+         //------------------------------------//
         if(left == -1 || right == -1) return -1;
         if(abs(left - right) > 1) return -1;
+        //-------------------------------------//
 
         return 1 + max(left, right);
     }
