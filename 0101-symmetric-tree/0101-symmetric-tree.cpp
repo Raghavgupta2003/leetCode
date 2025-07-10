9e3f7840c75fa29f72e412199d0312966759ad67
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-07-11 04:44:35 +0530

    Time: 0 ms (100%), Space: 18.3 MB (86.04%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -11,17 +11,22 @@
  */
 class Solution {
 public:
-    bool same(TreeNode* root1, TreeNode* root2){
-        if(root1 == NULL && root2 == NULL) return true;
-        if(root1 != NULL && root2 == NULL) return false;
-        if(root1 == NULL && root2 != NULL) return false;
-        return (root1->val == root2->val) && same(root1->left, root2->right) && same(root1->right, root2->left);
+    bool same(TreeNode* p, TreeNode* q){
+        if(p==NULL && q==NULL) return true;
+        if(p!=NULL && q==NULL) return false;
+        if(p==NULL && q!=NULL) return false;
+
+
+        if(p->val != q->val) return false;
+
+        bool left = same(p->left, q->right);
+        bool right = same(p->right, q->left);
+
+        return left && right;
     }
     bool isSymmetric(TreeNode* root) {
         if(root == NULL) return true;
-        TreeNode* root1 = root->left;
-        TreeNode* root2 = root->right;
 
-        return same(root1, root2);
+        return same(root->left, root->right);
     }
 };
