074384adba1da56f3ffc953cf80c9c3089bcb48a
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-07-12 04:37:50 +0530

    Time: 7 ms (28.98%), Space: 27.4 MB (75.29%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -11,24 +11,41 @@
  */
 class Solution {
 public:
-    TreeNode* helper(vector<int>& inorder, vector<int>& postorder, int &postIdx, int left,int right){
-        if(left > right) return NULL;
-        
-        TreeNode* cur = new TreeNode(postorder[postIdx]);
-        postIdx--;
+    //IMPORTANT POINTS
+    //In POST ORDER => last Index is root
+    //lastIndex-1 is root->right
+    //lastIndex-2 is root->left
 
-        //Searching in inorder
-        int idxInInorder = -1;
-        for(int i = left; i <= right; i++){
-            if(inorder[i] == cur->val) idxInInorder = i;
-        }
-        cur->right = helper(inorder, postorder, postIdx, idxInInorder+1, right);
-        cur->left = helper(inorder, postorder, postIdx, left, idxInInorder-1);
+    //so, our goal to find root node is done
+    //if we found same root node inorder traversal then we also get information about left and right
 
-        return cur;
+    TreeNode* make(vector<int>& inorder, vector<int>& postorder, int& postorderIdx, int inorderleft, int inorderright, unordered_map<int, int>& m){
+        if(inorderleft > inorderright) return NULL;
+
+        TreeNode* root = new TreeNode(postorder[postorderIdx]);
+        postorderIdx--;
+
+        int positionInOrder = m[root->val]; //we cannot access by postorderIdx as we decrement it
+
+        //now first we create right tree as postorder given
+
+        root->right = make(inorder, postorder, postorderIdx, positionInOrder + 1, inorderright, m);
+
+        root->left = make(inorder, postorder, postorderIdx, inorderleft ,positionInOrder - 1, m);
+
+        return root;
     }
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        int postIdx = postorder.size()-1;
-        return helper(inorder, postorder, postIdx, 0, inorder.size()-1);
+        //for effective Search in inorder,
+        //we hashmap it
+        //want to find index by element
+
+        unordered_map<int, int> m;
+        for(int i=0; i<inorder.size(); i++){
+            m[inorder[i]] = i;
+        }
+        
+        int postorderIdx = postorder.size()-1;
+        return make(inorder, postorder, postorderIdx, 0, inorder.size()-1, m);
     }
 };
