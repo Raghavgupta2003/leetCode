0bc9e66fa44fac09595ccf17330b7de673ac6c0a
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-07-12 01:08:40 +0530

    Time: 9 ms (79.49%), Space: 18.8 MB (8.64%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -7,63 +7,35 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-// class Solution {
-// public:
-//    // Applications -> Distance of two nodes
-//     bool path(vector<TreeNode*>& v, TreeNode* root, TreeNode* n){
-//         if(root == NULL) return false;
-
-//         if(root->val == n->val){
-//             v.push_back(root);
-//             return true;
-//         }
-
-//         if(path(v,root->left,n) || path(v,root->right,n)){
-//             v.push_back(root);
-//             return true;
-//         }
-
-//         return false;
-//     }
-//     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-//         vector<TreeNode*> v1;
-//         path(v1, root, p);
-//         vector<TreeNode*> v2;
-//         path(v2, root, q);
-        
-//         int i = v1.size()-1;
-//         int j = v2.size()-1;
-        
-//         while(i>=0 && j>=0){
-//             if(v1[i] == v2[j]){
-//                 i--;
-//                 j--;
-//             }
-//             else{
-//                 return v1[i+1];
-//             }
-//         }
-//         if(i<v1.size()) return v1[i+1];
-//         if(j<v2.size()) return v2[j+1];
-
-//         return NULL;
-//     }
-// };
-
-//-----------------------------------------------------------------------
-
 class Solution {
 public:
-   // Applications -> Distance of two nodes
+    // BRUTE FORCE : In this question, we find path to a node 
+    //and travel backward in it
+
+    bool pathToNode(TreeNode* root, TreeNode* node, vector<TreeNode*>& arr){
+        if(root == NULL) return false;
+        arr.push_back(root);
+        if(root == node) return true;
+   
+        bool found = pathToNode(root->left, node, arr) || pathToNode(root->right, node, arr);
+
+        if(found == false) arr.pop_back();
+
+        return found;
+    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root == NULL) return NULL;
+        vector<TreeNode*> arr1;
+        pathToNode(root, p, arr1);
 
-        if(p==root || q==root) return root;
+        vector<TreeNode*> arr2;
+        pathToNode(root, q, arr2);
 
-        TreeNode* left = lowestCommonAncestor(root->left, p, q);
-        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        for(int i=arr1.size()-1; i>=0; i--){
+            for(int j=arr2.size()-1; j>=0; j--){
+                if(arr1[i] == arr2[j]) return arr1[i];
+            }
+        }
 
-        if(left && right) return root;
-        return left ? left : right;
+        return NULL;
     }
 };
