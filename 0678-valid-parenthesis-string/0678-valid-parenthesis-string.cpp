54d10cbabeb95f63592b3e11f470d2774aebe6a0
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-07-12 07:06:47 +0530

    Time: 0 ms (100%), Space: 8.2 MB (46.99%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
+    // Here in this question, we play with indexes
+    // As we there is a * before close then we have to treat it like '('
     bool checkValidString(string s) {
-        //since (*) can give
-        // cnt = 0 => ""
-        // cnt = 1 => "("
-        // cnt = -1 =>")"
+        stack<int> st;
+        stack<int> star;
 
-        //since -1 is not possible for it, so we keep 2 pointers min and max here
-        //and perform with these
-        //at end any one is zero then return true;
-        
         if(s[0] == ')') return false;
-        int max = 0;
-        int min = 0;
         for(int i=0; i<s.size(); i++){
-            if(s[i] == '('){
-                max++;
-                min++;
-            }else if(s[i] == ')'){
-                max--;
-                min--;
+            if(s[i] == '(') st.push(i);
+            else if(s[i] == '*') star.push(i);
+            else if(s[i] == ')'){
+                if(st.size() > 0) st.pop();
+                else if(star.size() > 0) star.pop();
+                else return false; //st.size() == 0 && star.size()==0 
             }
-            else{
-                min--;
-                max++;
-            }
-
-            if(min<0) min = 0;
-            if(max < 0) return false;
         }
 
-        return min==0;
+        while(st.size()>0 && star.size()>0){
+            if(st.top() < star.top()){
+                st.pop();
+                star.pop();
+            }else return false;
+        }
+
+        return st.size() == 0;
     }
 };
