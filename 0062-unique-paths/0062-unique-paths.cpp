5d4208c2d412a4370d0a2e4b8c26d3b2b100ac42
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-07-14 09:33:28 +0530

    Time: 0 ms (100%), Space: 9.2 MB (57.33%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,18 +1,32 @@
 class Solution {
 public:
-    int paths(int i, int j, int m, int n, vector<vector<int>>& dp){
-        if(i == m-1 && j==n-1) return 1;
-        if(i>=m || j>=n) return 0;
+    // int paths(int i, int j, int m, int n, vector<vector<int>>& dp){
+    //     if(i == m-1 && j==n-1) return 1;
+    //     if(i>=m || j>=n) return 0;
 
-        if(dp[i][j]!=-1) return dp[i][j];
+    //     if(dp[i][j]!=-1) return dp[i][j];
 
-        int right = paths(i, j+1, m, n, dp);
-        int down = paths(i+1, j, m, n, dp);
+    //     int right = paths(i, j+1, m, n, dp);
+    //     int down = paths(i+1, j, m, n, dp);
 
-        return dp[i][j] = right + down;
+    //     return dp[i][j] = right + down;
+    // }
+
+    int paths(int m, int n, vector<vector<int>>& dp){
+        if(m == 0 && n==0) return 1;
+        if(m<0 || n<0) return 0;
+
+        if(dp[m][n]!=-1) return dp[m][n];
+
+        int left = paths(m, n-1, dp);
+        int up = paths(m-1, n, dp);
+
+        return dp[m][n] = left + up;
     }
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m, vector<int>(n, -1));
-        return paths(0, 0, m, n, dp);
+        // return paths(0, 0, m, n, dp);
+
+        return paths(m-1, n-1, dp);
     }
 };
