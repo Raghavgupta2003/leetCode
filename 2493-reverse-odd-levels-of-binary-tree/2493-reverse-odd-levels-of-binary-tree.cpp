c136765cf8ca9805f503678eeca42c97f97b4c8b
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-07-16 11:16:25 +0530

    Time: 4 ms (40.11%), Space: 82.1 MB (42.34%) - LeetHub

--- 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.cpp
+++ 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.cpp
@@ -11,47 +11,39 @@
  */
 class Solution {
 public:
-    TreeNode* createTree(int i ,vector<int> &arr){
-        if(i>=arr.size()) return NULL;
-
-        TreeNode* root = new TreeNode(arr[i]);
-        root->left = createTree(2*i + 1 ,arr);
-        root->right = createTree(2*i + 2 ,arr);
-
-        return root;
-    }
+   // we will store odd level nodes in vector
+   // and revere its values by while loop.
     TreeNode* reverseOddLevels(TreeNode* root) {
         if(root == NULL) return {};
 
-        vector<vector<int>> ans;
         queue<TreeNode*> q;
         q.push(root);
         int cnt = 0;
         while(q.size() >0){
             int size = q.size();
-            vector<int> help;
+            vector<TreeNode*> help;
             cnt++;
             for(int i=0; i<size; i++){
                 TreeNode* cur = q.front();
                 q.pop();
 
-                help.push_back(cur->val);
+                if(cnt % 2 == 0) help.push_back(cur); //only odd level
 
                 if(cur->left) q.push(cur->left);
                 if(cur->right) q.push(cur->right);
             }
-            if(cnt % 2 == 0) reverse(help.begin(), help.end());
-            ans.push_back(help);
-        }
-        
-        vector<int> arr;
-        for(int i=0; i<ans.size(); i++){
-            for(int j=0; j<ans[i].size(); j++){
-                arr.push_back(ans[i][j]);
-                cout<<ans[i][j]<<" ";
+            if(cnt % 2 == 0){
+                int i=0;
+                int j=help.size()-1;
+                while(i<=j){
+                    swap(help[i]->val, help[j]->val);
+                    i++;
+                    j--;
+                }
             }
         }
+        
 
-        return createTree(0 ,arr);
+        return root;
     }
 };
