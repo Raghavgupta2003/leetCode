32fd1999ed49789c006b59be311e47a9ff73ee84
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-07-19 02:15:28 +0530

    Time: 0 ms (100%), Space: 9.8 MB (76.49%) - LeetHub

--- 1127-last-stone-weight/1127-last-stone-weight.cpp
+++ 1127-last-stone-weight/1127-last-stone-weight.cpp
@@ -1,24 +1,56 @@
+// class Solution {
+// public:
+//     int lastStoneWeight(vector<int>& stones) {
+//         while(stones.size() > 1){
+//             sort(stones.begin(), stones.end());
+//             int n = stones.size();
+
+//             int x = stones[n-2];
+//             int y = stones[n-1];
+
+//             if(x==y){ //destroy
+//                 stones.pop_back();
+//                 stones.pop_back();
+//             }else{
+//                 //x<y
+//                 stones.pop_back();
+//                 stones.back() = y-x;
+//             }
+//         }
+
+//         if(stones.size() < 1) return 0;
+//         return stones[0];
+//     }
+// };
+
+
+//------------optimizing it by max heap as max elements are top--------------
+
+
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
-        while(stones.size() > 1){
-            sort(stones.begin(), stones.end());
-            int n = stones.size();
+        priority_queue<int> maxheap;
 
-            int x = stones[n-2];
-            int y = stones[n-1];
+        for(int i=0; i<stones.size(); i++){
+            maxheap.push(stones[i]);
+        }
+        ///here we need not to sort again and again heap auto sort
+        while(maxheap.size() > 1){
+            int y = maxheap.top();
+            maxheap.pop();
 
-            if(x==y){ //destroy
-                stones.pop_back();
-                stones.pop_back();
-            }else{
-                //x<y
-                stones.pop_back();
-                stones.back() = y-x;
+            int x = maxheap.top();
+            maxheap.pop();
+
+            if(x==y) continue;
+            else{
+                //x < y
+
+                maxheap.push(y-x);
             }
         }
-
-        if(stones.size() < 1) return 0;
-        return stones[0];
+        if(maxheap.size() < 1) return 0;
+        return maxheap.top();
     }
 };
