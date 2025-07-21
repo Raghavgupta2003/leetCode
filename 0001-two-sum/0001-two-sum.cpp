209da81c44fc1665f0336145f209325b3e584712
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-07-21 09:58:51 +0530

    Time: 2 ms (75.93%), Space: 14.5 MB (59.31%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,20 +1,36 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int> m;
-        vector<int> ans;
-        for(int i=0;i<nums.size();i++){
-           int x = target - nums[i];
+        vector<int> arr = nums;
+        sort(nums.begin(), nums.end());
+        int i=0;
+        int j=nums.size()-1;
 
-           if(m.find(x)!=m.end()){
-            ans.push_back(i);
-            ans.push_back(m[x]);
-           }
-           else{
-            m[nums[i]] = i;
-           }
+        int a = -1;
+        int b = -1;
+        while(i<=j){
+            if(nums[i] + nums[j] == target){
+                a = nums[i];
+                b = nums[j];
+                i++;
+                j--;
+                break;
+            }
+            else if(nums[i] + nums[j] < target){
+                i++;
+            }
+            else{
+                j--;
+            }
+        }
+        cout<<a<<" "<<b;
+        int k =-1;
+        int l = -1;
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr[i] == a && k == -1) k = i; // ✅ pick first occurrence of a
+            else if (arr[i] == b && l == -1) l = i; // ✅ pick first occurrence of b
         }
 
-        return ans;
+        return {k, l};
     }
 };
