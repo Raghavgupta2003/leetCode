d432fbc2346b623c569290ad75a26fb1f200862d
RAGHAV GUPTA <119725985+Raghavgupta2003@users.noreply.github.com>
2025-09-25 05:19:24 +0530

    Time: 979 ms (98.74%), Space: 0B (100%) - LeetHub

--- 1509-replace-employee-id-with-the-unique-identifier/1509-replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/1509-replace-employee-id-with-the-unique-identifier.sql
@@ -5,3 +5,6 @@ FROM Employees
 LEFT OUTER JOIN EmployeeUNI
 ON Employees.id = EmployeeUNI.id 
 
+-- ess question mai bolaa hai ki humme saaaare names chaiye isliye hmne left join lgaaya
+-- aur mention nhai jinki unique id nhi hai, wha null, toh humne id compare kr li.
+
